bc11b902021abcb699175b2e1f27d20d4d1ab72b
Dennis Priadka <dennis.priadka@gmail.com>
2020-07-24 21:00:13 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+(function videoplayer() {
+  const vid = document.querySelector('.movie__video');
+  const img = document.querySelector('.movie__img');
+  const play = document.querySelector('.movie__play-button');
+  const darker = document.querySelector('.movie__darker');
+
+  vid.onclick = function() {
+    vid.pause();
+    darker.style.display = 'block';
+    img.style.display = 'block';
+    play.style.display = 'block';
+  };
+
+  play.onclick = function() {
+    darker.style.display = 'none';
+    img.style.display = 'none';
+    play.style.display = 'none';
+    vid.play();
+  };
+})();
