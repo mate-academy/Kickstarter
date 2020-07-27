6e789c1abe38a4c971177763883992dab192578a
Alisa <aliceblunt8@gmail.com>
2020-07-27 11:33:49 +0300

    close mobile menu on click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const navList = document.querySelector('.header__nav-list');
+const toggler = document.querySelector('.toggler');
+
+navList.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  toggler.checked = false;
+
+  const anchor = event.target;
+  const sectionId = anchor.getAttribute('href');
+
+  document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+);
