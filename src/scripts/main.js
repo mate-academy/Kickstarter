85fe9bab401c2ec461edbacba35cc2d139c70bf7
Alisa <aliceblunt8@gmail.com>
2020-09-12 12:49:18 +0300

    add changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,33 @@
 'use strict';
 
-const navList = document.querySelector('.header__nav-list');
-const toggler = document.querySelector('.toggler');
+addStyleOnScroll();
+scrollToSection();
 
-navList.addEventListener('click', function(event) {
-  event.preventDefault();
+function addStyleOnScroll() {
+  window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav');
+    const fromTop = nav.getBoundingClientRect().top;
 
-  toggler.checked = false;
-
-  const anchor = event.target;
-  const sectionId = anchor.getAttribute('href');
-
-  document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+    if (fromTop === 0) {
+      nav.style.boxShadow = '0px 1px 0px 0px rgba(13,178,179,0.3)';
+    } else {
+      nav.style.boxShadow = '';
+    }
+  });
 }
-);
+
+function scrollToSection() {
+  const navList = document.querySelector('.header__nav-list');
+  const toggler = document.querySelector('.toggler');
+
+  navList.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    toggler.checked = false;
+
+    const anchor = event.target;
+    const sectionId = anchor.getAttribute('href');
+
+    document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+  });
+}
