e02a9365343bec0617e50fc6c8e09c4836830510
38063 <xeilen1112@gmail.com>
2020-09-16 18:40:31 +0300

    -added burger-menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const mobMenu = document.querySelector('.menu-mobile');
+
+const burger = document.querySelector('.burger');
+
+burger.onclick = function() {
+  this.classList.toggle('burger--active');
+
+  if (this.classList.contains('burger--active')) {
+    mobMenu.style.left = '0';
+    document.body.style.overflow = 'hidden';
+  } else {
+    mobMenu.style.left = '-100%';
+  }
+};
+
+mobMenu.onclick = function() {
+  burger.classList.toggle('burger--active');
+  mobMenu.style.left = '-100%';
+  document.body.style.overflow = 'visible';
+};
