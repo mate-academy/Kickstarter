fd09b4d85fcb01508d652dc8dc1cab3267823648
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-09-21 12:01:15 +0300

    shipping block created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+$('<div class="product-quantity-nav"><div class="product-quantity-button product-quantity-down">-</div><div class="product-quantity-button product-quantity-up">+</div></div>').insertAfter('.product-quantity input');
+$('.product-quantity').each(function() {
+  var spinner = $(this),
+    input = spinner.find('input[type="number"]'),
+    btnUp = spinner.find('.product-quantity-up'),
+    btnDown = spinner.find('.product-quantity-down'),
+    min = input.attr('min'),
+    max = input.attr('max');
+
+  btnUp.click(function() {
+    var oldValue = parseFloat(input.val());
+    if (oldValue >= max) {
+      var newVal = oldValue;
+    } else {
+      var newVal = oldValue + 1;
+    }
+    spinner.find("input").val(newVal);
+    spinner.find("input").trigger("change");
+  });
+
+  btnDown.click(function() {
+    var oldValue = parseFloat(input.val());
+    if (oldValue <= min) {
+      var newVal = oldValue;
+    } else {
+      var newVal = oldValue - 1;
+    }
+    spinner.find("input").val(newVal);
+    spinner.find("input").trigger("change");
+  });
+});
