f069432059d8c5030760e6538436133d76f7f524
38063 <xeilen1112@gmail.com>
2020-09-21 13:01:58 +0300

    -fixed after student-review and added video

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,10 @@ const mobMenu = document.querySelector('.menu-mobile');
 
 const burger = document.querySelector('.burger');
 
+const video = document.querySelector('.presentation__clip');
+
+const playBtn = document.querySelector('.presentation__play');
+
 burger.onclick = function() {
   this.classList.toggle('burger--active');
 
@@ -20,4 +24,10 @@ mobMenu.onclick = function() {
   burger.classList.toggle('burger--active');
   mobMenu.style.left = '-100%';
   document.body.style.overflow = 'visible';
+};
+
+playBtn.onclick = function() {
+  video.play();
+  // video.requestFullscreen();
+  playBtn.style.display = 'none';
 };
