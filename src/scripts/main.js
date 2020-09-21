c0dc3d1f882a14747874cfdd08bcd116926bb8c6
38063 <xeilen1112@gmail.com>
2020-09-21 20:18:03 +0300

    -fixed after mentor-review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@
 
 const mobMenu = document.querySelector('.menu-mobile');
 
+const links = document.querySelectorAll('.menu-mobile__item');
+
 const burger = document.querySelector('.burger');
 
 const video = document.querySelector('.presentation__clip');
@@ -12,22 +14,27 @@ burger.onclick = function() {
   this.classList.toggle('burger--active');
 
   if (this.classList.contains('burger--active')) {
-    mobMenu.style.left = '0';
+    mobMenu.style.transform = 'translate(-8%)';
     document.body.style.overflow = 'hidden';
   } else {
-    mobMenu.style.left = '-100%';
+    mobMenu.style.transform = 'translate(-108%)';
     document.body.style.overflow = 'visible';
   }
 };
 
-mobMenu.onclick = function() {
-  burger.classList.toggle('burger--active');
-  mobMenu.style.left = '-100%';
-  document.body.style.overflow = 'visible';
-};
+links.forEach(item => {
+  item.onclick = function() {
+    burger.classList.toggle('burger--active');
+    mobMenu.style.transform = 'translate(-108%)';
+    document.body.style.overflow = 'visible';
+  };
+});
 
 playBtn.onclick = function() {
   video.play();
-  // video.requestFullscreen();
   playBtn.style.display = 'none';
+};
+
+video.onpause = function() {
+  playBtn.style.display = 'flex';
 };
