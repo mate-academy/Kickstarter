e4bd1ef2bee496d1427aabcd985a8d4eb7ee6a67
goncharukop <goncharukop@gmail.com>
2020-09-21 17:00:56 +0300

    add tech-slider, js file, fix width overflow

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+document.querySelector('.burger-menu').onclick = () => {
+  document.querySelector('.burger-menu__toggler').checked = false;
+};
+
+document.querySelector('.video-btn').onclick = () => {
+  document.querySelector('.video__clip').play();
+  document.querySelector('.video-btn').style.display = 'none';
+};
+
+document.querySelector('.video__clip').onpause = () => {
+  document.querySelector('.video-btn').style.display = 'block';
+};
