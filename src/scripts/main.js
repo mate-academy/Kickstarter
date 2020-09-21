c159ea8afb2ddc3e2824b878f177da2bda896be5
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-09-21 22:59:21 +0300

    payment page created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,43 @@
 'use strict';
 
-$('<div class="product-quantity-nav"><div class="product-quantity-button product-quantity-down">-</div><div class="product-quantity-button product-quantity-up">+</div></div>').insertAfter('.product-quantity input');
-$('.product-quantity').each(function() {
-  var spinner = $(this),
-    input = spinner.find('input[type="number"]'),
-    btnUp = spinner.find('.product-quantity-up'),
-    btnDown = spinner.find('.product-quantity-down'),
-    min = input.attr('min'),
-    max = input.attr('max');
+/* $(
+  '<div class="product-quantity-nav">\n'
+  + '<div class="product-quantity-button product-quantity-down">-</div>\n'
+    + '<div class="product-quantity-button product-quantity-up">+</div>\n'
+    + '</div>').insertAfter('.product-quantity input');
 
-  btnUp.click(function() {
-    var oldValue = parseFloat(input.val());
-    if (oldValue >= max) {
-      var newVal = oldValue;
-    } else {
-      var newVal = oldValue + 1;
-    }
-    spinner.find("input").val(newVal);
-    spinner.find("input").trigger("change");
-  });
+$('.product-quantity').each(
+  function() {
+    const spinner = $(this);
+    const input = spinner.find('input[type="number"]');
+    const btnUp = spinner.find('.product-quantity-up');
+    const btnDown = spinner.find('.product-quantity-down');
+    const min = input.attr('min');
+    const max = input.attr('max');
 
-  btnDown.click(function() {
-    var oldValue = parseFloat(input.val());
-    if (oldValue <= min) {
-      var newVal = oldValue;
-    } else {
-      var newVal = oldValue - 1;
-    }
-    spinner.find("input").val(newVal);
-    spinner.find("input").trigger("change");
-  });
-});
+    btnUp.click(function() {
+      const oldValue = parseFloat(input.val());
+      let newVal = 0;
+
+      if (oldValue >= max) {
+        newVal = oldValue;
+      } else {
+        newVal = oldValue + 1;
+      }
+      spinner.find('input').val(newVal);
+      spinner.find('input').trigger('change');
+    });
+
+    btnDown.click(function() {
+      const oldValue = parseFloat(input.val());
+      let newVal = 0;
+
+      if (oldValue <= min) {
+        newVal = oldValue;
+      } else {
+        newVal = oldValue - 1;
+      }
+      spinner.find('input').val(newVal);
+      spinner.find('input').trigger('change');
+    });
+  }); */
