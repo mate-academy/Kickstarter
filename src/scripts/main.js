31646cd03e6aa800a6b909136d7f08463f98dc3e
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-09-23 11:04:10 +0300

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,52 @@
 'use strict';
 
+const menu = document.querySelector('.burger-menu');
+const btn = menu.querySelector('.burger-menu__nav-tgl');
+btn.addEventListener('click', evt => {
+	menu.classList.toggle('active');
+})
+
+const video = document.querySelector('.video-section__item'),
+      juise = document.querySelector('.video-section__red-juice'),
+      playBtn = document.getElementById('play-pause');
+
+function tagglePlayPause() {
+  if(video.paused) {
+    playBtn.className = 'pause';
+    video.play();
+  } 
+  else {
+    playBtn.className = 'play';
+    video.pause();
+  }
+}
+
+playBtn.onclick = function() {
+  tagglePlayPause();
+}
+
+let slider = tns({
+  container: '.about-slider',
+  items: 1,
+  responsive: {
+    650: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 1
+    },
+    700: {
+      gutter: 30
+    },
+    900: {
+      items: 3
+    }
+  }
+});
+
+
 /* $(
   '<div class="product-quantity-nav">\n'
-  + '<div class="product-quantity-button product-quantity-down">-</div>\n'
+  + '<div class="product-quantity-button product-quantity-down">-</div>\n'npm 
     + '<div class="product-quantity-button product-quantity-up">+</div>\n'
     + '</div>').insertAfter('.product-quantity input');
 
