10e80ff6e32815e736e28d26f47f904cc6e9bca1
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-12 14:21:47 +0300

    burger-menu fix and video,

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,40 @@
 'use strict';
 
+const menu = document.querySelector('.burger-menu');
+const btn = menu.querySelector('.burger-menu__nav-tgl');
+const link = document.querySelector('.burger-menu__nav');
+const video = document.querySelector('.video-section__item');
+const playBtn = document.querySelector('.video-section__play-button');
+
+btn.addEventListener('click', evt => {
+  menu.classList.toggle('active');
+});
+
+link.addEventListener('click', evt => {
+  menu.classList.remove('active');
+});
+
+playBtn.onclick = function() {
+  video.play();
+  playBtn.style.display = 'none';
+};
+
+video.onpause = function() {
+  playBtn.style.display = 'flex';
+};
+
 // eslint-disable-next-line no-undef
-$(
+document.querySelector(
   '<div class="product-quantity-nav">\n'
   + '<div class="product-quantity-button product-quantity-down">-</div>\n'
     + '<div class="product-quantity-button product-quantity-up">+</div>\n'
     + '</div>').insertAfter('.product-quantity input');
 
 // eslint-disable-next-line no-undef
-$('.product-quantity').each(
+document.querySelector('.product-quantity').each(
   function() {
     // eslint-disable-next-line no-undef
-    const spinner = $(this);
+    const spinner = document.querySelector(this);
     const input = spinner.find('input[type="number"]');
     const btnUp = spinner.find('.product-quantity-up');
     const btnDown = spinner.find('.product-quantity-down');
