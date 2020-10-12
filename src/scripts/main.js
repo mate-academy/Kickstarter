30c2e604fde34e9d12fa735515634dacdccbffe5
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-12 09:41:23 +0300

    general fix after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,64 +1,6 @@
 'use strict';
 
-<<<<<<< HEAD
-const menu = document.querySelector('.burger-menu');
-const btn = menu.querySelector('.burger-menu__nav-tgl');
-const body = document.querySelector('body');
-const bodyHidden = document.querySelector('body.hudden');
-const nav = document.querySelector('.burger-menu__nav');
-
-btn.addEventListener('click', evt => {
-  menu.classList.toggle('active');
-  body.classList.toggle('hidden');
-  bodyHidden.classList.toggle('active');
-});
-
-nav.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  body.classList.toggle('hidden');
-});
-
-const video = document.querySelector('.video-section__item');
-const playBtn = document.getElementById('play-pause');
-
-function tagglePlayPause() {
-  if (video.paused) {
-    playBtn.className = 'pause';
-    video.play();
-  } else {
-    playBtn.className = 'play';
-    video.pause();
-  }
-}
-
-playBtn.onclick = function() {
-  tagglePlayPause();
-};
-
 // eslint-disable-next-line no-undef
-const aboutSlider = tns({
-  container: '.about-slider',
-  items: 1,
-  responsive: {
-    640: {
-      edgePadding: 20,
-      gutter: 20,
-      items: 2,
-    },
-    700: {
-      gutter: 30,
-    },
-    900: {
-      items: 3,
-    },
-  },
-});
-
-aboutSlider();
-
-// eslint-disable-next-line no-undef
-=======
->>>>>>> c506129026c1dfe9426160ad0887882407de3534
 $(
   '<div class="product-quantity-nav">\n'
   + '<div class="product-quantity-button product-quantity-down">-</div>\n'
