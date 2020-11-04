eb8781d4d5b2afefdba72dc17bfd156be361e33a
andlap24 <andrew.laptev24@gmail.com>
2020-11-04 15:32:14 +0200

    implement burger menu on js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const mobMenu = document.querySelector('.mobile-nav');
+const links = document.querySelectorAll('.mobile-nav__item');
+const burger = document.querySelector('.burger');
+
+burger.onclick = function() {
+  this.classList.toggle('burger--active');
+
+  if (this.classList.contains('burger--active')) {
+    mobMenu.style.transform = 'translate(-8%)';
+    document.body.style.overflow = 'hidden';
+  } else {
+    mobMenu.style.transform = 'translate(-108%)';
+    document.body.style.overflow = 'visible';
+  }
+};
+
+links.forEach(item => {
+  item.onclick = function() {
+    burger.classList.toggle('burger--active');
+    mobMenu.style.transform = 'translate(-108%)';
+    document.body.style.overflow = 'visible';
+  };
+});
