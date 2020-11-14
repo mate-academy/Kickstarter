2cac5819b28699855264af8fc8b22892c0a3b65f
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-11-14 13:34:25 +0200

    syntax fix && unused code removed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const menu = document.querySelector('.burger-menu');
-const btn = menu.querySelector('.burger-menu__nav-tgl');
+const btn = document.querySelector('.burger-menu__nav-tgl');
 const link = document.querySelector('.burger-menu__nav');
 const video = document.querySelector('.video-section__item');
 const playBtn = document.querySelector('.video-section__play-button');
 const body = document.body;
 
-btn.addEventListener('click', evt => {
+btn.addEventListener('click', event => {
   menu.classList.toggle('active');
   body.style.overflow = 'hidden';
 });
 
-link.addEventListener('click', evt => {
+link.addEventListener('click', event => {
   menu.classList.remove('active');
   body.style.overflow = 'visible';
 });
@@ -25,47 +25,3 @@ playBtn.onclick = function() {
 video.onpause = function() {
   playBtn.style.display = 'flex';
 };
-
-document.querySelector(
-  '<div class="product-quantity-nav">\n'
-  + '<div class="product-quantity-button product-quantity-down">-</div>\n'
-    + '<div class="product-quantity-button product-quantity-up">+</div>\n'
-    + '</div>').insertAfter('.product-quantity input');
-
-// eslint-disable-next-line no-undef
-document.querySelector('.product-quantity').each(
-  function() {
-    // eslint-disable-next-line no-undef
-    const spinner = document.querySelector(this);
-    const input = spinner.find('input[type="number"]');
-    const btnUp = spinner.find('.product-quantity-up');
-    const btnDown = spinner.find('.product-quantity-down');
-    const min = input.attr('min');
-    const max = input.attr('max');
-
-    btnUp.click(function() {
-      const oldValue = parseFloat(input.val());
-      let newVal = 0;
-
-      if (oldValue >= max) {
-        newVal = oldValue;
-      } else {
-        newVal = oldValue + 1;
-      }
-      spinner.find('input').val(newVal);
-      spinner.find('input').trigger('change');
-    });
-
-    btnDown.click(function() {
-      const oldValue = parseFloat(input.val());
-      let newVal = 0;
-
-      if (oldValue <= min) {
-        newVal = oldValue;
-      } else {
-        newVal = oldValue - 1;
-      }
-      spinner.find('input').val(newVal);
-      spinner.find('input').trigger('change');
-    });
-  });
