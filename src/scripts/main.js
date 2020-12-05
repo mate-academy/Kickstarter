31838a2daf3d582f327b240d2d783245b909db9e
Alex Lemeshchuk <alexyskr@gmail.com>
2020-12-05 16:19:19 +0200

    Added navbar for desktop, tablets, mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const menu = document.querySelector('.navbar');
+const toggler = document.querySelector('.menu__toggler');
+const buy = document.querySelector('.menu__buy');
+
+toggler.onclick = function toggleSidebar() {
+  toggler.classList.toggle('menu__toggler--active');
+  buy.classList.toggle('menu__buy--active');
+  menu.classList.toggle('navbar--open');
+};
