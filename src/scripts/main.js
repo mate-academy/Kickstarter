74b1fbb28394007ab070bd9f7dca1726b2bac021
Alex Lemeshchuk <alexyskr@gmail.com>
2020-12-06 13:52:55 +0200

    Added fixes for main menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,13 @@
 const menu = document.querySelector('.navbar');
 const toggler = document.querySelector('.menu__toggler');
 const buy = document.querySelector('.menu__buy');
+const brand = document.querySelector('.menu__brand-link');
+const header = document.querySelector('body');
 
 toggler.onclick = function toggleSidebar() {
   toggler.classList.toggle('menu__toggler--active');
   buy.classList.toggle('menu__buy--active');
   menu.classList.toggle('navbar--open');
+  brand.classList.toggle('menu__brand-link--open');
+  header.classList.toggle('header--open');
 };
