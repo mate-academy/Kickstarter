6f2b3b74c2d0b6820bbebefc58bcec772a93daec
Artem Maksimikhin <uanov86@gmail.com>
2021-02-19 21:50:23 +0200

    add function for sliders and second slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,48 @@
 'use strict';
 
-let slideIndex = 1;
+function slider(btnPrev, btnNext, sliderItem) {
+  let slideIndex = 1;
 
-showSlides(slideIndex);
+  showSlides(slideIndex);
 
-const prev = document.querySelector('.info-slider__prev');
-const next = document.querySelector('.info-slider__next');
+  const next = document.querySelector(`${btnNext}`);
+  const prev = document.querySelector(`${btnPrev}`);
 
-next.onclick = function() {
-  showSlides(slideIndex += 1);
-};
+  next.addEventListener('click', () => {
+    showSlides(slideIndex += 1);
+  });
 
-prev.onclick = function() {
-  showSlides(slideIndex -= 1);
-};
+  prev.addEventListener('click', () => {
+    showSlides(slideIndex -= 1);
+  });
 
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
+  function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName(`${sliderItem}`);
 
-function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName('info-slider__item');
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
 
-  if (n > slides.length) {
-    slideIndex = 1;
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = 'block';
   }
-
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slides[slideIndex - 1].style.display = 'block';
 }
+
+const infoPrev = '.info-slider__prev';
+const infoNext = '.info-slider__next';
+const infoSlides = 'info-slider__item';
+
+slider(infoPrev, infoNext, infoSlides);
+
+const featuresPrev = '.features-slider__prev';
+const featuresNext = '.features-slider__next';
+const featuresSlides = 'features-slider__item';
+
+slider(featuresPrev, featuresNext, featuresSlides);
