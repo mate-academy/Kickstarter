bcea8409b00c88dd196d6d2de8ab045dc309ec60
Artem Maksimikhin <uanov86@gmail.com>
2021-02-23 17:09:10 +0200

    finish mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// slider
+
 function slider(btnPrev, btnNext, sliderItem) {
   let slideIndex = 1;
 
@@ -46,3 +48,28 @@ const featuresNext = '.features-slider__next';
 const featuresSlides = 'features-slider__item';
 
 slider(featuresPrev, featuresNext, featuresSlides);
+
+// end slider
+
+// burger-menu
+
+const burgerMenuWrap = document.querySelector('.burger-menu-wrapper');
+const burgerMenu = document.querySelector('.burger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileNav = document.querySelector('.mobile-nav__list');
+
+burgerMenuWrap.onclick = function() {
+  burgerMenu.classList.toggle('burger-menu--active');
+  burgerMenuWrap.classList.toggle('burger-menu-wrapper--active');
+  mobileMenu.classList.toggle('mobile-menu--active');
+  document.body.classList.toggle('body--disabled');
+};
+
+mobileNav.onclick = function() {
+  burgerMenu.classList.remove('burger-menu--active');
+  burgerMenuWrap.classList.remove('burger-menu-wrapper--active');
+  document.body.classList.remove('body--disabled');
+  mobileMenu.classList.remove('mobile-menu--active');
+};
+
+// burger-menu end
