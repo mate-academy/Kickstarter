2286090a0dffde2fb0af4f954604c3a27e4c1a1c
kshvetsova <katshvetsova@gmail.com>
2021-03-02 06:39:59 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function setStyleNavSroll() {
+  window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav');
+    const top = nav.getBoundingClientRect().top;
+
+    if (top === 0) {
+      nav.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.05)';
+    } else {
+      nav.style.boxShadow = '';
+    }
+  });
+};
+setStyleNavSroll();
