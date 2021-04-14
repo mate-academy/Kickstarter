04b90ac6e17787c3f5406e9266a95000869cfb70
Nazar Matsevych <nazar6192111@gmail.com>
2021-04-14 15:26:32 +0300

    1/6 ready

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Kickstarter landing page
-
+-[DEMO LINK](https://nazarmatsevych.github.io/Kickstarter/)
 Implement landing page according to [Figma design](https://www.figma.com/file/5jdcVOv7NiA0l0HGfqEyHC/%E2%84%9611-(kickstarter)-(Copy)?node-id=0%3A1) - Use BEM and SCSS
 - The design 1440px
 - Notebook 1280px
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const burgerToggler = document.querySelector('.burger-toggler');
+const nav = document.querySelector('.nav-burger');
+const button = document.querySelector('#burgerToggler');
+
+button.addEventListener('click', () => {
+  burgerToggler.classList.toggle('burger-toggler--active');
+  nav.classList.toggle('nav-burger--active');
+});
