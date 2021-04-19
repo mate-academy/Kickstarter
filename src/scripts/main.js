10a6dc2a0f844bf86ad66e1121aa2cfaf455f35b
Nikolay Butylkov <kokos.ibo.58@gmail.com>
2021-04-19 13:43:42 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const toggler = document.querySelector('.presentation__img-play');
+
+toggler.addEventListener('click', () => {
+  toggler.classList.toggle('img-play--active');
+});
