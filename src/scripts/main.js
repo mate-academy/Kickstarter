3ad145ef8c546ccafc2fc917b41881c90942d3a8
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-06-18 13:40:53 +0300

    js implemented & minor fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const checkbox = document.querySelector('.burger__toggle');
+const menu = document.querySelector('.burger__list');
+
+checkbox.addEventListener('click', () => {
+  menu.classList.toggle('burger__list--active');
+  checkbox.classList.toggle('burger__toggle--active');
+});
+
+menu.addEventListener('click', () => {
+  menu.classList.remove('burger__list--active');
+  checkbox.classList.remove('burger__toggle--active');
+});
