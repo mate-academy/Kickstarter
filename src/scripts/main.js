50b97f503867e4aab4856ec723e400e6f5f551da
Andrii-Kalaida <harleyarkhem@gmail.com>
2021-07-27 12:59:34 +0300

    commit for test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function changeLanguages() {
+  const activeClass = 'header__nav-button--active';
+  const buttons = [...document.getElementsByClassName('js-language-button')];
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(el => {
+        el.classList.remove(activeClass);
+      });
+      button.classList.add(activeClass);
+    });
+  });
+}
+
+window.addEventListener('load', () => {
+  changeLanguages();
+});
