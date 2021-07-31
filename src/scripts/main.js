0c4c93693a250768b5203e99a7887900bb30d07e
Niko Lazur <lazur.niko@gmail.com>
2021-07-31 13:54:54 +0300

    fixed sliders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+$(document).ready(function(){
+  $('.description-slider').slick({
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+})
