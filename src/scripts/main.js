9599dcd2220e40fac4cfa9ce072e19a9edc7f3ea
Maksym <wolfsangel009@gmail.com>
2021-08-02 14:08:16 +0300

    bug fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const subNavButtons = document.querySelectorAll('.sub-nav__link');
+
+subNavButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    subNavButtons.forEach(btn =>
+      btn.classList.remove('sub-nav__link--is--active'));
+    this.classList.add('sub-nav__link--is--active');
+  });
+});
