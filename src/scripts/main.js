2f6ed3591f52da1c6a7c9eb99d34968ae3a9a46e
Niko Lazur <lazur.niko@gmail.com>
2021-08-02 15:29:55 +0300

    fixed responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,1 @@
 'use strict';
-$(document).ready(function(){
-  $('.description-slider').slick({
-    slidesToScroll: 1,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-  });
-})
