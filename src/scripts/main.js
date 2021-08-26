db23a101c84746b54c1cb850c8ccc4cc94d37c95
Kirill Ivanchenko <ivanchenkok557@gmail.com>
2021-08-26 23:22:19 +0300

    Some fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const playButton = document.querySelector('.presentation__play-btn');
+const video = document.querySelector('.presentation__video');
+
+playButton.addEventListener('click', () => {
+  video.classList.remove('presentation__video--hidden');
+  playButton.classList.remove('presentation__play-btn');
+});
