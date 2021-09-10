c5134d0929b2f8312b7b13acf28b3ce43b3e85e4
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-09-10 17:20:14 +0300

    layout_kickstarter fixed bugs

--- src/scripts/mobile-menu.js
+++ src/scripts/mobile-menu.js
@@ -2,7 +2,7 @@
 
 const body = document.querySelector('.page__body');
 const menuOpener = document.querySelector('.header__menu--menu-opener');
-const menuCloser = document.querySelector('.menu__closer');
+const menuCloser = document.querySelector('.menu');
 
 menuOpener.addEventListener('click', () => {
   body.classList.add('page__body--with-menu');
