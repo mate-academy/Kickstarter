90ea7189243a4a6a04dff6de45f24d6d924a4996
Kostiantyn Besedin <besconstantine@gmail.com>
2021-09-12 19:56:55 +0300

    added second slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,7 +27,7 @@ lang.forEach((item) => {
   });
 });
 
-// slider out story
+// slider our story
 
 const stories = document.querySelector('.our-story__wrapper');
 const story = document.querySelectorAll('.our-story__story');
@@ -41,6 +41,7 @@ function moveStory(shift) {
   if (shift > 0 && positionStory <= countStory - 2) {
     positionStory += shift;
   }
+
   if (shift < 0 && positionStory > 0) {
     positionStory += shift;
   }
@@ -59,7 +60,7 @@ function moveStory(shift) {
 
   story.forEach((item) => {
     item.style.transform = `translateX(${-positionStory * 100}%)`;
-  })
+  });
 }
 
 rightStory.addEventListener('click', () => {
@@ -68,4 +69,48 @@ rightStory.addEventListener('click', () => {
 
 leftStory.addEventListener('click', () => {
   moveStory(-1);
+});
+
+// slider features
+
+const features = document.querySelector('.features__wrapper');
+const feature = document.querySelectorAll('.feature');
+const leftFeature = document.querySelector('.features__left');
+const rightFeature = document.querySelector('.features__right');
+const countFeature = features.children.length;
+
+let positionFeature = 0;
+
+function moveFeature(shift) {
+  if (shift > 0 && positionFeature <= countFeature - 2) {
+    positionFeature += shift;
+  }
+
+  if (shift < 0 && positionFeature > 0) {
+    positionFeature += shift;
+  }
+
+  if (positionFeature >= countFeature - 1) {
+    rightFeature.classList.remove('features__right--active');
+  } else {
+    rightFeature.classList.add('features__right--active');
+  }
+
+  if (positionFeature <= 0) {
+    leftFeature.classList.remove('features__left--active');
+  } else {
+    leftFeature.classList.add('features__left--active');
+  };
+
+  feature.forEach((item) => {
+    item.style.transform = `translateX(${-positionFeature * 100}%)`;
+  });
+}
+
+rightFeature.addEventListener('click', () => {
+  moveFeature(1);
+});
+
+leftFeature.addEventListener('click', () => {
+  moveFeature(-1);
 });
