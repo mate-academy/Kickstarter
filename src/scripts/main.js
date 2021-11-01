94225d060616772aaff212dd44a829af1f351789
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-11-01 15:17:08 +0200

    first version with support of iphone11, ipad, desktop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#side-menu') {
+    document.body.classList.add('nav__side-menu--no-scroll');
+  } else {
+    document.body.classList.remove('nav__side-menu--no-scroll');
+  }
+});
