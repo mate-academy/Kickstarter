cd431b41e53d94b0d833ea1cda1ae2ef82d4c99e
unknown <skirik0117@gmail.com>
2021-11-01 19:32:05 +0200

    create-page-crazybaby

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const video = document.querySelector('.presentation__video');
+
+const playButton = document
+  .querySelector('.presentation__button-play');
+const pauseButton = document
+  .querySelector('.presentation__button-pause');
+
+playButton.addEventListener('click', () => {
+  video.play();
+  playButton.style.display = 'none';
+  pauseButton.style.display = 'block';
+});
+
+pauseButton.addEventListener('click', () => {
+  video.pause();
+  playButton.style.display = 'block';
+  pauseButton.style.display = 'none';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
