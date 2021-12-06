b3a13773c317318bca5a2a4871eebccad07424bc
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2021-12-06 10:57:55 +0200

    Kickstarter - alpha version - v0.00, only mobile adaptability

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const wrapper = document.querySelector('.wrapper');
+const toggler = document.querySelector('.menu__btn');
+
+toggler.addEventListener('click', function() {
+  wrapper.classList.toggle('wrapper--open');
+});
