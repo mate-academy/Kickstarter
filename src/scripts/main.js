8ee6048d81fd8f36bdfd53cf17f14fd5536fa10f
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2021-12-07 11:07:55 +0200

    Alpha version - v0.85

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,17 @@
 
 const wrapper = document.querySelector('.wrapper');
 const toggler = document.querySelector('.menu__btn');
+const form = document.querySelector('.questions__form');
+const inputs = document.querySelectorAll('.questions__field');
 
 toggler.addEventListener('click', function() {
   wrapper.classList.toggle('wrapper--open');
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const elem of inputs) {
+    elem.value = '';
+  }
 });
