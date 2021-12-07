79f93b25787856fc61d206ad7b315d7b9753d3e8
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2021-12-07 11:32:03 +0200

    Alpha version - 0.85 - micro-fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,19 @@
 'use strict';
 
 const wrapper = document.querySelector('.wrapper');
-const toggler = document.querySelector('.menu__btn');
-const form = document.querySelector('.questions__form');
-const inputs = document.querySelectorAll('.questions__field');
+const toggler = document.querySelector('.top-bar__btn');
 
 toggler.addEventListener('click', function() {
   wrapper.classList.toggle('wrapper--open');
 });
+
+const form = document.querySelector('.questions__form');
+const inputs = document.querySelectorAll('.questions__field');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   for (const elem of inputs) {
     elem.value = '';
-  }
+  };
 });
