5c1e22e3a52a5159574b349eef060433619c521d
Serhii Haidukov <sergeyglazgo@gmail.com>
2021-12-08 21:38:38 +0200

    babyland

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+/* eslint-disable */
+
+const form = document.querySelector('.questions__form');
+const inputs = document.querySelectorAll('.questions__input');
+const textarea = document.querySelector('.questions__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(elem => elem.value = '');
+
+  textarea.value = '';
+});
