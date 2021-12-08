3f8fa5b280fc5d38d247ce210eb805417c20c01d
levix2l <alexleviz13@gmail.com>
2021-12-08 22:42:01 +0200

    v0.95

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu-phone') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contact-us__form');
+const email = document.querySelector('.contact-us__field--email');
+const textarea = document.querySelector('.contact-us__field--textarea');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  email.value = '';
+  textarea.value = '';
+});
