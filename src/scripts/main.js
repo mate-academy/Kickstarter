a92a8e4ca19a0df65fe279c3acec7dd82d80b261
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2021-12-10 21:55:04 +0200

    Beta version - v2.10 - add js input validator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,13 +7,27 @@ toggler.addEventListener('click', function() {
   wrapper.classList.toggle('menu--open');
 });
 
-const form = document.querySelector('.questions__form');
-const inputs = document.querySelectorAll('.questions__field');
+function getDynamicInformation(selector) {
+  const input = document.querySelector(selector);
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+  input.addEventListener('input', () => {
+    if (input.value.match(/[@]/g)) {
+      input.style.border = '1px solid green';
+    } else {
+      input.style.border = '1px solid red';
+    }
+  });
 
-  for (const elem of inputs) {
-    elem.value = '';
-  }
-});
+  const form = document.querySelector('.questions__form');
+  const inputs = document.querySelectorAll('.questions__field');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    for (const elem of inputs) {
+      elem.value = '';
+      input.style.border = '';
+    }
+  });
+}
+getDynamicInformation('.questions__field');
