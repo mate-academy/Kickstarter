f9476d5bcabb982feb07e9b6d1065d9c2034ce5b
Sesile26 <37846431+Sesile26@users.noreply.github.com>
2021-12-13 16:14:41 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,19 @@ form.addEventListener('submit', (event) => {
     input.value = '';
   }
 });
+
+const presentationVideo = document.querySelector('.presentation__img');
+const buttonStart = document.querySelector('.presentation__play');
+const buttonStop = document.querySelector('.presentation__stop');
+
+buttonStart.addEventListener('click', () => {
+  presentationVideo.play();
+  buttonStart.style.display = 'none';
+  buttonStop.style.display = 'block';
+});
+
+buttonStop.addEventListener('click', () => {
+  presentationVideo.pause();
+  buttonStart.style.display = 'block';
+  buttonStop.style.display = 'none';
+});
