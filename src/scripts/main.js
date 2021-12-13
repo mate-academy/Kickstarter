522bac441d9bc250ad91c7064f48be2087ecf297
Sesile26 <37846431+Sesile26@users.noreply.github.com>
2021-12-13 07:08:37 +0200

    sulotion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.question__form');
+const inputs = document.querySelectorAll('.question__field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
