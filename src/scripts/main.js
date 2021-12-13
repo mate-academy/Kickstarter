7f7cf855057b39dc11511623c3f5735983ee650e
levix2l <alexleviz13@gmail.com>
2021-12-13 13:51:22 +0200

    header text fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,13 +8,13 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-// const form = document.querySelector('.contact-us__form');
-// const inputs = document.querySelectorAll('.contact-us__field');
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__field');
 
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-//   for (const input of inputs) {
-//     input.value = '';
-//   }
-// });
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
