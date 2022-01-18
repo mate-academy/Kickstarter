5a241e7c6f4dc72e2777ee3710d78fa9170229f7
Roma Boreiko <roma.boreiko@gmail.com>
2022-01-18 16:45:07 +0200

    some adjustments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+// function toggle() {
+//   const presentationPopup = document.querySelector('.presentation__popup');
+
+//   presentationPopup.classList.toggle('presentation__popup--active');
+// }
