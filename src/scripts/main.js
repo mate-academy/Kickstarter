96302ca8e4796a583cc6625ea9ee8c725de328c2
Artem <zuller123@gmail.com>
2022-02-05 15:30:10 +0200

    fix nav and header display styles, impruved fonts and adapt layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const storyVideo = document.querySelector('.story__video');
+const storyButtonPlay = document.querySelector('.story__button--play');
+const storyButtonPause = document.querySelector('.story__button--pause');
+
+storyButtonPlay.addEventListener('click', () => {
+  storyVideo.play();
+  storyButtonPlay.style.display = 'none';
+  storyButtonPause.style.display = 'flex';
+});
+
+storyButtonPause.addEventListener('click', () => {
+  storyVideo.pause();
+  storyButtonPlay.style.display = 'flex';
+  storyButtonPause.style.display = 'none';
+});
