187d15c69c33e7ba23a95b5ea6828b23f24dff88
Artem <zuller123@gmail.com>
2022-02-08 15:12:10 +0200

    created features section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// #video section
+
 const storyVideo = document.querySelector('.story__video');
 const storyButtonPlay = document.querySelector('.story__button--play');
 const storyButtonPause = document.querySelector('.story__button--pause');
@@ -15,3 +17,58 @@ storyButtonPause.addEventListener('click', () => {
   storyButtonPlay.style.display = 'flex';
   storyButtonPause.style.display = 'none';
 });
+
+// #slider section
+
+const sliderItems = document.querySelector('.slider__items');
+const sliderItem = document.querySelector('.slider__item');
+
+const sliderPrevButton = document.querySelector('.slider__button--pre');
+const sliderNextButton = document.querySelector('.slider__button--next');
+
+const sliderCurrentPage = document.querySelector('.slider__page--now');
+const sliderCountPage = document.querySelector('.slider__page--last');
+
+const slideAmount = sliderItems.children.length;
+
+const slideWidth = sliderItem.offsetWidth;
+
+updatePageNumber(slideAmount, sliderCountPage);
+
+let currentPage = 0;
+
+sliderNextButton.addEventListener('click', () => {
+  disableButton(currentPage - 1);
+
+  currentPage--;
+  updatePageNumber(-currentPage + 1);
+
+  sliderItems.style.transform = `translateX(${currentPage * slideWidth}px)`;
+  sliderPrevButton.removeAttribute('disabled');
+});
+
+sliderPrevButton.addEventListener('click', () => {
+  disableButton(currentPage + 1);
+
+  currentPage++;
+  updatePageNumber(-currentPage + 1);
+
+  sliderItems.style.transform = `translateX(${currentPage * slideWidth}px)`;
+  sliderNextButton.removeAttribute('disabled');
+});
+
+function disableButton(page) {
+  if (page + 1 > 0) {
+    sliderPrevButton.setAttribute('disabled', '');
+  }
+
+  if (page - 1 === -slideAmount) {
+    sliderNextButton.setAttribute('disabled', '');
+  }
+}
+
+function updatePageNumber(page, element = sliderCurrentPage, numLength = 2) {
+  element.textContent = `${
+    ''.repeat(numLength - String(page).length)}${page}
+  `;
+}
