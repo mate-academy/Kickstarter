269865789c8d54b6864811992984dde11f360dd0
Artem <zuller123@gmail.com>
2022-02-09 13:11:50 +0200

    fixed scroll under menu, hover styles for icons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -81,3 +81,12 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   form.reset();
 });
+
+// #no scroll under menu
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
