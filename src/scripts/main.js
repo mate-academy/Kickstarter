b53bd841f5f11b4e8278a2113daea86f7f5293a5
Alexxs <irreligious86@gmail.com>
2022-02-14 03:29:55 +0200

    improve popup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const langSwitcher
+  = document.querySelectorAll('.lang-switch');
+
+const changeLang = () => {
+  const langItems = document.querySelectorAll('.lang-case');
+
+  langItems.forEach(i => {
+    i.classList.toggle('lang-case-active');
+  });
+};
+
+langSwitcher.forEach(i => {
+  i.addEventListener('click', (e) => {
+    e.preventDefault();
+    changeLang();
+  });
+});
