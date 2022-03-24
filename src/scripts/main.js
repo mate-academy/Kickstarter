71db077b71c00e166df72d1e668b29d9cbac984a
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-03-24 17:57:50 +0200

    nav hovers changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function play(id) {
+  const video = document.querySelector(id);
+
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+const button = document.querySelector('#video-presentation-button');
+
+button.addEventListener('click', function handleClick() {
+  play('#video-presentation');
+});
