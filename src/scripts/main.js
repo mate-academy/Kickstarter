71b58995afe3b6a6cf97a98709f01518b1c3db1c
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-03-28 16:57:27 +0300

    features slider implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,3 +19,56 @@ button.addEventListener('change', function handleClick() {
     video.pause();
   }
 });
+
+const featuresPrev = document.querySelector('#features-prev');
+const featuresNext = document.querySelector('#features-next');
+const featuresCurrent = document.querySelector('#features-current-frame');
+
+function swipeFeatures(offset) {
+  const togglers = document.querySelectorAll('.features__frame-toggler');
+
+  for (let i = 0; i < togglers.length; i++) {
+    const toggler = togglers[i];
+
+    if (toggler.checked
+      && i + offset < togglers.length
+      && i + offset >= 0) {
+      toggler.checked = false;
+      togglers[i + offset].checked = true;
+
+      const indexFormat = '00';
+      let displayIndex = i + 1 + offset;
+
+      displayIndex += indexFormat + displayIndex.toString();
+      featuresCurrent.innerHTML = displayIndex.slice(-indexFormat.length);
+
+      if (offset > 0) {
+        featuresPrev.classList.add('slider__active-arrow');
+
+        if (i + offset === togglers.length - 1) {
+          featuresNext.classList.remove('slider__active-arrow');
+        }
+      }
+
+      if (offset < 0) {
+        featuresNext.classList.add('slider__active-arrow');
+
+        if (i + offset === 0) {
+          featuresPrev.classList.remove('slider__active-arrow');
+        }
+      }
+
+      break;
+    }
+  }
+
+  return 0;
+};
+
+featuresPrev.addEventListener('click', () => {
+  swipeFeatures(-1);
+});
+
+featuresNext.addEventListener('click', () => {
+  swipeFeatures(1);
+});
