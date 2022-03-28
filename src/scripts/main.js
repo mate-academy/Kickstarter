192dff931d7c842a21f9cd1a23dc5f4746cbc8cf
Kirill Polozenko <kir.polozenko@icloud.com>
2022-03-28 17:50:03 +0300

    fixed menu and form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--scroll');
+  } else {
+    document.body.classList.remove('page__body--scroll');
+  }
+});
+
 function stopDefAction(event) {
   document.getElementById('form').reset();
   event.preventDefault();
