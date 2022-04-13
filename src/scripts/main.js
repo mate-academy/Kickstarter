2aefcdfac921fe847e63c8bd88688ff2e7ba2692
Swazi Kunene <kuneneswazi18@gmail.com>
2022-04-13 23:30:02 +0200

    added slides for mobile using js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,3 +15,36 @@ togglerOff.addEventListener('click', () => {
   menu.style.transform = 'translateX(-100%)';
   document.body.classList.remove('page--with-menu');
 });
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+
+const number = document.getElementById('number');
+
+next.addEventListener('click', () => {
+  if (one.style.display !== 'none') {
+    one.style.display = 'none';
+    two.style.display = 'block';
+    number.innerHTML = '02';
+  } else if (two.style.display !== 'none') {
+    two.style.display = 'none';
+    three.style.display = 'block';
+    number.innerHTML = '03';
+  }
+});
+
+prev.addEventListener('click', () => {
+  if (two.style.display !== 'none') {
+    two.style.display = 'none';
+    one.style.display = 'block';
+    number.innerHTML = '01';
+  } else if (three.style.display !== 'none') {
+    three.style.display = 'none';
+    two.style.display = 'block';
+    number.innerHTML = '02';
+  }
+});
