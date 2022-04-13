6cf8c8c9fe2ad982c8e3d5e04bd8dea82741bc9a
Swazi Kunene <kuneneswazi18@gmail.com>
2022-04-13 15:45:43 +0200

    added testimonials section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const togglerOn = document.getElementById('menuOn');
+const togglerOff = document.getElementById('menuOff');
+const menu = document.getElementById('menu');
+const menuNav = document.getElementById('menuNav');
+
+togglerOn.addEventListener('click', () => {
+  menu.style.transform = 'translateX(0)';
+  menuNav.style.display = 'contents';
+  document.body.classList.add('page--with-menu');
+});
+
+togglerOff.addEventListener('click', () => {
+  menu.style.transform = 'translateX(-100%)';
+  document.body.classList.remove('page--with-menu');
+});
