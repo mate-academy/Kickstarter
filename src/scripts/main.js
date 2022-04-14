c65138e8d6cfceeed8a7a8c8fb1cc47febf5cc94
Swazi Kunene <kuneneswazi18@gmail.com>
2022-04-15 00:13:20 +0200

    updated sliding menu, still got minor issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,8 +11,14 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
+// sliding features js
+const prevInactive = document.getElementById('prevInactive');
+const prevActive = document.getElementById('prevActive');
+const prevHover = document.getElementById('prevHover');
+
+const nextInactive = document.getElementById('nextInactive');
+const nextActive = document.getElementById('nextActive');
+const nextHover = document.getElementById('nextHover');
 
 const featureOne = document.getElementById('featureOne');
 const featureTwo = document.getElementById('featureTwo');
@@ -20,30 +26,59 @@ const featureThree = document.getElementById('featureThree');
 
 const featureNumber = document.getElementById('featureNumber');
 
-next.addEventListener('click', () => {
+nextActive.addEventListener('mouseover', () => {
+  nextActive.style.display = 'none';
+  nextHover.style.display = 'inline-block';
+});
+
+nextHover.addEventListener('mouseout', () => {
+  nextActive.style.display = 'inline-block';
+  nextHover.style.display = 'none';
+});
+
+nextHover.addEventListener('click', () => {
   if (featureOne.style.display !== 'none') {
     featureOne.style.display = 'none';
     featureTwo.style.display = 'block';
     featureNumber.innerHTML = '02';
+    prevActive.style.display = 'inline-block';
+    prevInactive.style.display = 'none';
   } else if (featureTwo.style.display !== 'none') {
     featureTwo.style.display = 'none';
     featureThree.style.display = 'block';
     featureNumber.innerHTML = '03';
+    nextActive.style.display = 'none';
+    nextInactive.style.display = 'inline-block';
   }
 });
 
-prev.addEventListener('click', () => {
+prevActive.addEventListener('mouseover', () => {
+  prevActive.style.display = 'none';
+  prevHover.style.display = 'inline-block';
+});
+
+prevHover.addEventListener('mouseout', () => {
+  prevActive.style.display = 'inline-block';
+  prevHover.style.display = 'none';
+});
+
+prevHover.addEventListener('click', () => {
   if (featureTwo.style.display !== 'none') {
     featureTwo.style.display = 'none';
     featureOne.style.display = 'block';
     featureNumber.innerHTML = '01';
+    prevActive.style.display = 'none';
+    prevInactive.style.display = 'inline-block';
   } else if (featureThree.style.display !== 'none') {
     featureThree.style.display = 'none';
     featureTwo.style.display = 'block';
     featureNumber.innerHTML = '02';
+    nextInactive.style.display = 'none';
+    nextActive.style.display = 'inline-block';
   }
 });
 
+// form reset js
 const form = document.getElementById('form');
 
 form.addEventListener('submit', function(event) {
@@ -51,6 +86,7 @@ form.addEventListener('submit', function(event) {
   form.reset();
 });
 
+// play and pause video js
 const play = document.getElementById('play');
 const video = document.getElementById('video');
 
