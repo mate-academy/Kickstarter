19fb0ee31ffc0a46fde30e3c6e6c78a3e7fe5fe8
Andrei Sucman <sucmanandrei@gmail.com>
2022-04-14 19:59:10 +0300

    added contact form and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const buttonLeft = document.getElementById('m-block__button-left');
-const buttonRight = document.getElementById('m-block__button-right');
-const paginationLeft = document.getElementById('m-block__pagination-left');
-const paginationRight = document.getElementById('m-block__pagination-right');
-
-
+// const buttonLeft = document.getElementById('m-block__button-left');
+// const buttonRight = document.getElementById('m-block__button-right');
+// const paginationLeft = document.getElementById('m-block__pagination-left');
+// const paginationRight = document.getElementById('m-block__pagination-right');
