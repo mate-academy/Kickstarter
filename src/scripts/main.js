f8acba59c30886bfc1bfeba6358315b32fcb3975
Swazi Kunene <kuneneswazi18@gmail.com>
2022-04-14 12:42:28 +0200

    added questions section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,50 +1,45 @@
 'use strict';
 
-const togglerOn = document.getElementById('menuOn');
-const togglerOff = document.getElementById('menuOff');
-const menu = document.getElementById('menu');
 const menuNav = document.getElementById('menuNav');
 
-togglerOn.addEventListener('click', () => {
-  menu.style.transform = 'translateX(0)';
-  menuNav.style.display = 'contents';
-  document.body.classList.add('page--with-menu');
-});
-
-togglerOff.addEventListener('click', () => {
-  menu.style.transform = 'translateX(-100%)';
-  document.body.classList.remove('page--with-menu');
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+    menuNav.style.display = 'inline-block';
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
 });
 
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
+const featureOne = document.getElementById('featureOne');
+const featureTwo = document.getElementById('featureTwo');
+const featureThree = document.getElementById('featureThree');
 
-const number = document.getElementById('number');
+const featureNumber = document.getElementById('featureNumber');
 
 next.addEventListener('click', () => {
-  if (one.style.display !== 'none') {
-    one.style.display = 'none';
-    two.style.display = 'block';
-    number.innerHTML = '02';
-  } else if (two.style.display !== 'none') {
-    two.style.display = 'none';
-    three.style.display = 'block';
-    number.innerHTML = '03';
+  if (featureOne.style.display !== 'none') {
+    featureOne.style.display = 'none';
+    featureTwo.style.display = 'block';
+    featureNumber.innerHTML = '02';
+  } else if (featureTwo.style.display !== 'none') {
+    featureTwo.style.display = 'none';
+    featureThree.style.display = 'block';
+    featureNumber.innerHTML = '03';
   }
 });
 
 prev.addEventListener('click', () => {
-  if (two.style.display !== 'none') {
-    two.style.display = 'none';
-    one.style.display = 'block';
-    number.innerHTML = '01';
-  } else if (three.style.display !== 'none') {
-    three.style.display = 'none';
-    two.style.display = 'block';
-    number.innerHTML = '02';
+  if (featureTwo.style.display !== 'none') {
+    featureTwo.style.display = 'none';
+    featureOne.style.display = 'block';
+    featureNumber.innerHTML = '01';
+  } else if (featureThree.style.display !== 'none') {
+    featureThree.style.display = 'none';
+    featureTwo.style.display = 'block';
+    featureNumber.innerHTML = '02';
   }
 });
