281bbcaa55550a3ed00c95151894dcb2ad7aa164
Andrei Sucman <sucmanandrei@gmail.com>
2022-04-14 21:18:37 +0300

    added links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
+});
 // const buttonLeft = document.getElementById('m-block__button-left');
 // const buttonRight = document.getElementById('m-block__button-right');
 // const paginationLeft = document.getElementById('m-block__pagination-left');
