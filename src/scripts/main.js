42ef4160886f144ad92363e6acd1d5db10947b31
Andrei Sucman <sucmanandrei@gmail.com>
2022-04-14 18:56:59 +0300

    added testimonial and features sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const buttonLeft = document.getElementById('m-block__button-left');
+const buttonRight = document.getElementById('m-block__button-right');
+const paginationLeft = document.getElementById('m-block__pagination-left');
+const paginationRight = document.getElementById('m-block__pagination-right');
+
+
