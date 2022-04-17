e359f44fbd2cf5f78b58be86e5717eef1ec75925
Zubov Illia <contactduodiamonds@gmail.com>
2022-04-17 20:21:25 +0300

    added slider Feauters

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let offset = 0;
+const sliderLine = document.querySelector('#slider-line');
+
+document.querySelector('#btn-forward').addEventListener('click', function() {
+  offset -= 270;
+
+  if (offset < -540) {
+    offset = 0;
+  }
+
+  sliderLine.style.left = offset + 'px';
+
+});
+
+document.querySelector('#btn-back').addEventListener('click', function() {
+  offset += 270;
+
+  if (offset > 0) {
+    offset = -540;
+  }
+
+  sliderLine.style.left = offset + 'px';
+});
