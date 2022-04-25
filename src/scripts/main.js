4bdfd3ad76c2951102dd72e2b8cb4387d140fa42
Nazar Muliar <nazarmuliar26@gmail.com>
2022-04-25 12:59:55 +0300

    improved hovers and refactored some blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-document.querySelector('#form').addEventListener('submit', function(event) {
-  event.preventDefault();
-}, false);
