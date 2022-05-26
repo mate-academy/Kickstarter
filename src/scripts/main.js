7e949d78bbc09ffd48d27f9bc9db80b45eca32f9
Nikita Karbazin <karbazinnikita@gmail.com>
2022-05-26 21:50:02 +0300

    4* blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let offset = 0;
+const sliderline = document.querySelector('.features__line');
+
+document.querySelector('.features__next')
+  .addEventListener('click', function() {
+    offset = offset + 214;
+    sliderline.style.top = -offset + 'px';
+  });
+
+document.querySelector('.features__prev')
+  .addEventListener('click', function() {
+    offset = offset - 214;
+    sliderline.style.top = -offset + 'px';
+  });
