b2333b49ca8c7b2708aa1f096f7e607612dc734a
Max Dumiak <maxdumiak39@gmail.com>
2022-05-27 23:51:16 +0300

    section 1 for mobile ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
+/* eslint-disable no-unused-vars */
 'use strict';
+
+// eslint-disable-next-line no-undef
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
