c9c27f51960e1a1add71eef8025274814328d263
Dmytro Kaunin <kaunin1198@te.net.ua>
2022-05-28 01:08:03 +0300

    Fourth commit / intermediate

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const video = document.querySelector('.presentation__video');
+const splash = document.querySelector('.presentation__splash-screen');
+const buttonPlay = document.querySelector('.presentation__play');
+const buttonPause = document.querySelector('.presentation__stop-button');
+
+buttonPlay.addEventListener('click', () => {
+  splash.style.display = 'none';
+  video.style.display = 'block';
+  video.play();
+  buttonPlay.style.display = 'none';
+  buttonPause.style.display = 'block';
+});
+
+buttonPause.addEventListener('click', () => {
+  video.pause();
+
+  splash.style.display = 'block';
+  buttonPlay.style.display = 'block';
+  buttonPause.style.display = 'none';
+});
