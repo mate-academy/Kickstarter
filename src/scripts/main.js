3d504fd024e1b1bf3db89585ae75ab7ea7886acd
Dmytro Kaunin <kaunin1198@te.net.ua>
2022-05-30 02:00:52 +0300

    Ninth commit / 90%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,3 +20,36 @@ buttonPause.addEventListener('click', () => {
   buttonPlay.style.display = 'block';
   buttonPause.style.display = 'none';
 });
+
+// eslint-disable-next-line no-unused-vars
+const slider = createSlider(document.querySelector('#slider'));
+
+// eslint-disable-next-line no-shadow
+function createSlider(slider) {
+  const list = slider.querySelector('.slider__content');
+  const counter = slider.querySelector('.slider__counter');
+  const prev = slider.querySelector('.slider__arrow--back');
+  const next = slider.querySelector('.slider__arrow--fore');
+
+  let position = 0;
+
+  function move(shift) {
+    const count = list.children.length;
+
+    position = (position + shift + count) % count;
+
+    counter.textContent = `${position + 1}`;
+
+    list.style.transform = `translateX(${-position * 100}%)`;
+  }
+
+  next.addEventListener('click', function() {
+    move(1);
+  });
+
+  prev.addEventListener('click', function() {
+    move(-1);
+  });
+
+  return { move };
+}
