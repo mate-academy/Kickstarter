23cc9ccaf700e65825749440fe9e8d1d00ccc122
Nazar Muliar <nazarmuliar26@gmail.com>
2022-05-30 16:50:10 +0300

    fixed on-submit form behavior

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,23 @@
 'use strict';
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('hashchange', () => {
+    if (window.location.hash === '#menu') {
+      document.body.classList.add('page__body--with-menu');
+    } else {
+      document.body.classList.remove('page__body--with-menu');
+    }
+  });
+
+  const form = document.querySelector('#form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const inputs = document.querySelectorAll('.form-field__field');
+
+    inputs.forEach(input => {
+      input.value = '';
+    });
+  });
 });
