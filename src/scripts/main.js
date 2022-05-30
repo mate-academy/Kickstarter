c62f3b8e4ab94bb10025f3db5d5783686ab106e4
Max Dumiak <maxdumiak39@gmail.com>
2022-05-30 09:06:44 +0300

    full mobile version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,39 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 'use strict';
 
-// eslint-disable-next-line no-undef
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper__systems', {
   loop: true,
 
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper__systems-pagination',
     clickable: true,
   },
+});
 
-  // Navigation arrows
+const swiper2 = new Swiper('.swiper__features', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper__features-pagination',
+    clickable: true,
+    type: 'fraction',
+    renderFraction: function (currentClass, totalClass) {
+      return '0<span class="' + currentClass + '"></span>'
+        + '<div class="swiper-pagination-wrapper"> / 0<span class="'
+        + totalClass + '"></span></div>';
+    },
+  },
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+window.addEventListener('scroll', function() {
+  const arrow = document.querySelector('.page__arrow-up');
+
+  arrow.classList.toggle('page__arrow-up--active', window.scrollY > 100);
 });
