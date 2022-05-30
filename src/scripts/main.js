60375a9bcab2d8799443d5e68b4762e2985fdfa4
Andrii Nychaiuk <anichajuk@gmail.com>
2022-05-30 14:50:13 +0300

    swiper added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const slider = document.querySelector('.slider__content');
+const prevButton = document.querySelector('.slider__button-prev');
+const nextButton = document.querySelector('.slider__button-next');
+
+let curentIndex = 0;
+
+function move(newIndex) {
+  if (newIndex < 0) {
+    curentIndex = 2;
+  } else if (newIndex > 2) {
+    curentIndex = 0;
+  } else {
+    curentIndex = newIndex;
+  }
+}
+
+nextButton.addEventListener('click', () => {
+  move(curentIndex + 1);
+  render();
+});
+
+prevButton.addEventListener('click', () => {
+  move(curentIndex - 1);
+  render();
+});
+
+function render() {
+  slider.style.transform = `translateX(${curentIndex * -(100)}%`;
+}
