2ab76a0182e7b2a90c94bac4d64bcb0b75114597
Dima Belotskyi <dimonbelotskiy@gmail.com>
2022-06-01 13:24:56 +0300

    Minor changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -43,3 +43,30 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+/* eslint-disable */
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  effect: 'flip',
+  simulateTouch: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
