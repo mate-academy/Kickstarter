e756aa5805a554b3fe99844ab93bd5bf6394d0c2
Nikita Karbazin <karbazinnikita@gmail.com>
2022-06-01 15:03:44 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,3 +28,23 @@ next.addEventListener('click', function() {
 prev.addEventListener('click', function() {
   move(currentIndex - 1);
 });
+
+window.onscroll = function() {
+  myFunction();
+};
+
+// Get the navbar
+const navbar = document.getElementById('desktop');
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its
+// scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
