bc2b8d8f4f0f479cd921e7b64fe30607799d1b4a
Bogdan Pyshchyk <bohdan.pyshchyk@gmail.com>
2022-06-04 15:24:41 +0300

    New commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const forma = document.getElementById('form');
+
+forma.addEventListener('submit', function(event) {
+  event.preventDefault();
+  forma.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
