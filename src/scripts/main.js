9b6f70de110249b3204867dee2cca362ede74a64
Bogdan Pyshchyk <bohdan.pyshchyk@gmail.com>
2022-06-05 11:33:02 +0300

    commit JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,21 @@
-'use strict';
+'use strict'
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use([ Navigation, Pagination ]);
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 const forma = document.getElementById('form');
 
@@ -14,3 +31,25 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('body--with-menu');
   }
 });
+
+const video_wrapper = document.querySelector('.about__wrapper');
+const video = document.getElementById('video');
+const splash = document.querySelector('.about__movie');
+const buttonPlay = document.querySelector('.about__movie-round');
+
+buttonPlay.addEventListener('click', () => {
+  splash.style.display = 'none';
+  video_wrapper.style.display = 'flex';
+  simulateClick(video);
+  buttonPlay.style.display = 'none';
+});
+
+function simulateClick(element){
+  trigger( element, 'mousedown' );
+  trigger( element, 'click' );
+  trigger( element, 'mouseup' );
+
+  function trigger( elem, event ) {
+    elem.dispatchEvent( new MouseEvent( event ) );
+  }
+}
