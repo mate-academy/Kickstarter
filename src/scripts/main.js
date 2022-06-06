93e3efca42203d6dac06a6d4ee1a5f37db756c27
Anton Shaga <toxa1381@gmail.com>
2022-06-06 08:28:14 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const list = document.querySelector('.features__slider');
+const buttonL = document.querySelector('.features__button--left');
+const buttonR = document.querySelector('.features__button--right');
+
+buttonL.addEventListener('click', () => {
+  list.style.transform = `translateX(-100%)`;
+});
+
+buttonR.addEventListener('click', () => {
+  list.style.transform = `translateX(100%)`;
+});
