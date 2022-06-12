c014548fd821e0c685dd5c092a0acd4d8dce4c47
Nazar Muliar <nazarmuliar26@gmail.com>
2022-06-12 18:29:06 +0300

    implemented slider for benefits section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,4 +20,45 @@ document.addEventListener('DOMContentLoaded', () => {
       input.value = '';
     });
   });
+
+  const benefits = [...document.querySelectorAll('.benefit')];
+  const slider = document.querySelector('.slider');
+  let slideIndexToShow = 0;
+
+  slider.addEventListener('click', (e) => {
+    const shouldSlideRight = e.target.dataset.direction === 'next'
+    && slideIndexToShow < benefits.length - 1;
+    const shouldSlideLeft = e.target.dataset.direction === 'prev'
+    && slideIndexToShow > 0;
+
+    if (shouldSlideRight) {
+      slideIndexToShow++;
+    } else if (shouldSlideLeft) {
+      slideIndexToShow--;
+    }
+
+    for (let i = 0; i < benefits.length; i++) {
+      if (i === slideIndexToShow) {
+        benefits[i].style.display = 'block';
+      } else {
+        benefits[i].style.display = 'none';
+      }
+    }
+  });
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+      benefits.forEach(benefit => {
+        if (benefit.style.display === 'none') {
+          benefit.style.display = 'block';
+        }
+      });
+    } else if (window.innerWidth < 768) {
+      benefits.forEach((benefit, i) => {
+        if (i !== 0 && benefit.style.display === 'block') {
+          benefit.style.display = 'none';
+        }
+      });
+    }
+  });
 });
