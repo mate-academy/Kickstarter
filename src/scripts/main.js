675686cce2bcabcff7244d2b9599da23943246a1
Oleksii Budnik <budnikoleksii@gmail.com>
2022-06-16 20:22:20 +0300

    added hover effects for menu links, button to the burger-menu and fixed some spaces in burger-menu, also hide button-top for first 1200px from top

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,13 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const pageUp = document.querySelector('.page__page-up-btn');
+
+window.addEventListener('scroll', () => {
+  if (document.documentElement.scrollTop > 1200) {
+    pageUp.classList.add('active');
+  } else {
+    pageUp.classList.remove('active');
+  }
+});
