ad92d73ce118b6047650140e0f25421df50a0c20
Oleksii Budnik <budnikoleksii@gmail.com>
2022-06-17 17:51:18 +0300

    worked on slider, have some problems with positioin for slides

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,3 +17,55 @@ window.addEventListener('scroll', () => {
     pageUp.classList.remove('active');
   }
 });
+
+// Slider
+const slider = function(sliderSelector,
+  slidesSelector, leftBtnSelector, rightBtnSelector) {
+  const sliderWrapper = document.querySelector(sliderSelector);
+  const slides = document.querySelectorAll(slidesSelector);
+  const btnLeft = document.querySelector(leftBtnSelector);
+  const btnRight = document.querySelector(rightBtnSelector);
+
+  let currentSlide = 0;
+  const maxSlide = slides.length;
+
+  sliderWrapper.style.width = 110 * slides.length + 'vw';
+
+  const goToSlide = function(slide) {
+    slides.forEach(
+      (s) => (s.style.transform = `translateX(${90 * (-slide)}vw)`)
+    );
+  };
+
+  const changeSlide = function(slide) {
+    goToSlide(slide);
+  };
+
+  const nextSlide = function() {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+
+    changeSlide(currentSlide);
+  };
+
+  const prevSlide = function() {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide - 1;
+    } else {
+      currentSlide--;
+    }
+
+    changeSlide(currentSlide);
+  };
+
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+};
+
+slider('.slider-about',
+  '.slider-about__slide',
+  '.slider-about__btn--left',
+  '.slider-about__btn--right');
