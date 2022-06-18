c4023616974e4cb2216c805fb5fb665bbc22b202
Oleksii Budnik <budnikoleksii@gmail.com>
2022-06-18 19:15:08 +0300

    added dots for slider, still have an issue with slides positions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,16 +20,38 @@ window.addEventListener('scroll', () => {
 
 // Slider
 const slider = function(sliderSelector,
-  slidesSelector, leftBtnSelector, rightBtnSelector) {
+  slidesSelector, leftBtnSelector, rightBtnSelector, dotsBoxSelector) {
   const sliderWrapper = document.querySelector(sliderSelector);
   const slides = document.querySelectorAll(slidesSelector);
   const btnLeft = document.querySelector(leftBtnSelector);
   const btnRight = document.querySelector(rightBtnSelector);
+  const dotsContainer = document.querySelector(dotsBoxSelector);
 
   let currentSlide = 0;
   const maxSlide = slides.length;
 
   sliderWrapper.style.width = 110 * slides.length + 'vw';
+
+  const createDots = function() {
+    slides.forEach((_, i) =>
+      dotsContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      )
+    );
+  };
+
+  const activateDot = function(slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach((dot) => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  createDots();
 
   const goToSlide = function(slide) {
     slides.forEach(
@@ -38,8 +60,11 @@ const slider = function(sliderSelector,
   };
 
   const changeSlide = function(slide) {
+    activateDot(slide);
     goToSlide(slide);
   };
+
+  changeSlide(currentSlide);
 
   const nextSlide = function() {
     if (currentSlide === maxSlide - 1) {
@@ -63,9 +88,18 @@ const slider = function(sliderSelector,
 
   btnRight.addEventListener('click', nextSlide);
   btnLeft.addEventListener('click', prevSlide);
+
+  dotsContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+
+      changeSlide(slide);
+    }
+  });
 };
 
 slider('.slider-about',
   '.slider-about__slide',
   '.slider-about__btn--left',
-  '.slider-about__btn--right');
+  '.slider-about__btn--right',
+  '.dots');
