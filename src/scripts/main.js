ccf80e52848b030502fd6fc668ed65489e687833
Oleksìi <bardyshev@gmail.com>
2022-06-23 21:21:33 +0300

    Scroll to top button was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,9 @@
 'use strict';
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
-
 const pageUp = document.querySelector('.page__page-up-btn');
 
 window.addEventListener('scroll', () => {
-  if (document.documentElement.scrollTop > 1200) {
+  if (document.documentElement.scrollTop > 600) {
     pageUp.classList.add('active');
   } else {
     pageUp.classList.remove('active');
