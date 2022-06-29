7e6a853f0221108cf82bd7a0438afc2afbbd41da
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-06-29 13:34:29 +0300

    Fixing code style, variables, animations. Added scroll to top

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -48,3 +48,16 @@ prevButton.addEventListener('click', e => {
 
   AddRemoveClass(currentSlide, nextSlide);
 });
+
+// Scroll Top
+const scrollTopButton = document.querySelector('.scrollTop');
+
+document.addEventListener('scroll', () => {
+  if (document.documentElement.scrollTop > 500) {
+    scrollTopButton.style.pointerEvents = 'auto';
+    scrollTopButton.style.opacity = '1';
+  } else {
+    scrollTopButton.style.opacity = '0';
+    scrollTopButton.style.pointerEvents = 'none';
+  }
+});
