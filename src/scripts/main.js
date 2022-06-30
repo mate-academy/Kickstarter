3fa52065481f4960d92088e33e1acf38dd6c2884
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-06-30 15:44:46 +0300

    hovers were added to buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+// let offset = 0;
+// const sliderLine = document.querySelector('.slider-line');
+
+// document.querySelector('.slider-next').addEventListener('click', function() {
+//   offset = offset + 400;
+//   if (offset > 900) {
+//     offset = 0;
+//   }
+//   sliderLine.style.left = -offset + 'px'
+// });
+
+// document.querySelector('.slider-prev').addEventListener('click', function() {
+//   offset = offset - 400;
+//   if (offset < 0) {
+//     offset = 1200;
+//   }
+//   sliderLine.style.left = -offset + 'px'
+// });
+
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('.questions__field');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
