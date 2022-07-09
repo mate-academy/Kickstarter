73c91e3e402245ab31b5317a7d557936ae69ab9a
Gena Kupriiuk <gamesgolden04@gmail.com>
2022-07-09 21:05:13 +0300

    features slider created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -40,3 +40,39 @@ form.addEventListener('submit', function(event) {
 
   form.reset();
 });
+
+const slider = document.querySelector('.slider__content');
+const prevButton = document.querySelector('.slider__button-prev');
+const nextButton = document.querySelector('.slider__button-next');
+
+let curentIndex = 0;
+
+function move(newIndex) {
+  if (newIndex < 0) {
+    curentIndex = 2;
+  } else if (newIndex > 2) {
+    curentIndex = 0;
+  } else {
+    curentIndex = newIndex;
+  }
+}
+
+nextButton.addEventListener('click', () => {
+  move(curentIndex + 1);
+  render();
+  pageCounter();
+});
+
+prevButton.addEventListener('click', () => {
+  move(curentIndex - 1);
+  render();
+  pageCounter();
+});
+
+function render() {
+  slider.style.transform = `translateX(${curentIndex * -(100)}%`;
+}
+
+function pageCounter() {
+  document.getElementById('slider__page').innerHTML = `0${curentIndex + 1}`;
+}
