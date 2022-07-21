f68337fdd3d7ea5e60c231046b5496e0e833d025
Vitalii Lola <92501117+G37ua@users.noreply.github.com>
2022-07-21 17:54:34 +0300

    crazybaby 001

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+
+//   inputs.forEach(function(elem) {
+//     elem.value = '';
+//   });
+
+//   textarea.value = '';
+// });
