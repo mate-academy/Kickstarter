90914a24d2337c2564136e3c80742c332acbd2e6
Pelahiia Khalishkhova <pelahiia.kh@gmail.com>
2022-07-25 19:44:16 +0200

    new version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+const myForm = document.querySelector('form');
+
+myForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const inputs = document.querySelectorAll('.form-field');
+inputs.forEach((input) => {
+  input.value = '';
+});
+});
+
+
