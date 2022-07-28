5f4a757723a5090518fb58f7e7e289ffc39bb580
Olya Yevsieienko <olya.evsnko@gmail.com>
2022-07-28 08:10:19 +0300

    Fixed bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,6 +10,8 @@ window.addEventListener('hashchange', () => {
   }
 });
 
+// Presentation
+
 const playButton = document.querySelector('.presentation__play-btn');
 const video = document.querySelector('.presentation__video');
 
@@ -18,89 +20,8 @@ playButton.addEventListener('click', () => {
   playButton.classList.remove('presentation__play-btn');
 });
 
-// Slider
-
-initSlider1(document.querySelector('.slider--1'));
-initSlider2(document.querySelector('.slider--2'));
-
-function initSlider1(sliderElement1) {
-  const slider = sliderElement1.querySelector('.slider__list');
-  const prevButton = sliderElement1.querySelector('.slider__button-prev');
-  const nextButton = sliderElement1.querySelector('.slider__button-next');
-
-  let currentIndex = 0;
-
-  function move(newIndex) {
-    if (newIndex < 0) {
-      currentIndex = 3;
-    } else if (newIndex > 3) {
-      currentIndex = 0;
-    } else {
-      currentIndex = newIndex;
-    }
-  }
-
-  nextButton.addEventListener('click', () => {
-    move(currentIndex + 1);
-    render();
-    pageCounter();
-  });
-
-  prevButton.addEventListener('click', () => {
-    move(currentIndex - 1);
-    render();
-    pageCounter();
-  });
-
-  function render() {
-    slider.style.transform = `translateX(${currentIndex * -(100)}%`;
-  }
-
-  function pageCounter() {
-    document.getElementById('slider__page1').innerHTML = `0${currentIndex + 1}`;
-  }
-}
-
-function initSlider2(sliderElement2) {
-  const slider = sliderElement2.querySelector('.slider__list');
-  const prevButton = sliderElement2.querySelector('.slider__button-prev');
-  const nextButton = sliderElement2.querySelector('.slider__button-next');
-
-  let currentIndex = 0;
-
-  function move(newIndex) {
-    if (newIndex < 0) {
-      currentIndex = 2;
-    } else if (newIndex > 2) {
-      currentIndex = 0;
-    } else {
-      currentIndex = newIndex;
-    }
-  }
-
-  nextButton.addEventListener('click', () => {
-    move(currentIndex + 1);
-    render();
-    pageCounter();
-  });
-
-  prevButton.addEventListener('click', () => {
-    move(currentIndex - 1);
-    render();
-    pageCounter();
-  });
-
-  function render() {
-    slider.style.transform = `translateX(${currentIndex * -(100)}%`;
-  }
-
-  function pageCounter() {
-    document.getElementById('slider__page2').innerHTML = `0${currentIndex + 1}`;
-  }
-}
-
 // Form
-const form = document.querySelector('form');
+const form = document.querySelector('.questions__form');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
