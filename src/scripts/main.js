6b23b0ff32d4bc4ce734e8ff428a8e98111ab7b3
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-07-29 00:53:52 +0300

    updated about-us, technology blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const aboutUsH = document.querySelector('.about-us__headline');
+const aboutUsPctr = document.querySelector('.about-us__pctr');
+
+if (window.matchMedia('(max-width: 767px)').matches) {
+  aboutUsH.appendChild(aboutUsPctr);
+}
