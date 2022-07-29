37893f96edcaced3d436a7c0129552975b8156eb
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-07-29 18:03:46 +0300

    Auto slider added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu-is-open');
+  } else {
+    document.body.classList.remove('page__body--menu-is-open');
+  }
+});
+
+const sliderAutoWrapper = document.querySelector('.slider-auto__wrapper');
+let offset = 0;
+
+function sliderAuto() {
+  offset += 100;
+
+  if (offset > 300) {
+    offset = 0;
+  }
+  sliderAutoWrapper.style.left = -offset + '%';
+}
+
+setInterval(sliderAuto, 8000);
