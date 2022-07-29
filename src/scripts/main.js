4644aa4dc9cc7ad33ee196ca8181ea5e8bb35570
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-07-29 16:01:07 +0300

    update desktop v

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,19 @@
 
 const aboutUsH = document.querySelector('.about-us__headline');
 const aboutUsPctr = document.querySelector('.about-us__pctr');
+const technologyH = document.querySelector('.technology__headline');
+const technologyPctr = document.querySelector('.technology__pctr');
+const featuresSection = document.querySelectorAll('.feature__section');
 
 if (window.matchMedia('(max-width: 767px)').matches) {
   aboutUsH.appendChild(aboutUsPctr);
+  technologyH.appendChild(technologyPctr);
+}
+
+if (window.matchMedia('(max-width: 1023px)').matches) {
+  for (let i = 0; i < featuresSection.length; i++) {
+    if (i !== featuresSection.length - 1) {
+      featuresSection[i].style.display = 'none';
+    }
+  }
 }
