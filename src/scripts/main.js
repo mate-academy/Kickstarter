8d9f3dbc75c91840d26981e8cd0dc6a6436b4724
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-07-30 01:17:12 +0300

    action menu link

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,7 @@ const aboutUsPctr = document.querySelector('.about-us__pctr');
 const technologyH = document.querySelector('.technology__headline');
 const technologyPctr = document.querySelector('.technology__pctr');
 const featuresSection = document.querySelectorAll('.feature__section');
+const scrollUp = document.querySelector('.scroll-up');
 
 if (window.matchMedia('(max-width: 767px)').matches) {
   aboutUsH.appendChild(aboutUsPctr);
@@ -18,3 +19,23 @@ if (window.matchMedia('(max-width: 1023px)').matches) {
     }
   }
 }
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20
+    || document.documentElement.scrollTop > 20) {
+    scrollUp.style.display = 'block';
+  } else {
+    scrollUp.style.display = 'none';
+  }
+}
+
+const menuLink = [...document.querySelectorAll('.menu__link')];
+
+menuLink.map(a => a.addEventListener('click', function() {
+  menuLink.map(b => b.classList.remove('menu__link--is-active'));
+  a.classList.add('menu__link--is-active');
+}));
