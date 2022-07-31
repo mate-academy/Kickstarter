b3aeb81bb2d385d8457046dd24216579d4a529b8
Dima Kobzar <kobzar7dima@gmail.com>
2022-07-31 23:14:37 +0300

    slider try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,8 +26,44 @@ window.addEventListener('hashchange', () => {
 //   item.addEventListener('click', (target) => {
 //     // console.log("object");
 //     // console.log(target);
-//     if (!target.classList.contains('switchers__switch--active')) {
+//     if (target.classList.contains('switchers__switch--active')) {
 //       target.classList.toggle('switchers__switch--active');
 //     }
 //   });
 // });
+
+// swiper попытки
+// const swiper = new Swiper('.mySwiper', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'fraction',
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+
+// это через импорты
+// core version + navigation, pagination modules:
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// // init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   // configure Swiper to use modules
+//   modules: [Navigation, Pagination],
+//   ...
+// });
+
+// через модули
+
+// // import Swiper JS
+// import Swiper from 'swiper';
+// // import Swiper styles
+// import 'swiper/css';
+
+// const swiper = new Swiper(...);
