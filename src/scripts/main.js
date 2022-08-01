025c72abea1d623f5ebd82866f435b5dc28a3f5a
Pelahiia Khalishkhova <pelahiia.kh@gmail.com>
2022-08-01 15:14:14 +0200

    fixed version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 'use strict';
+
 const myForm = document.querySelector('form');
 
-myForm.addEventListener("submit", function(event) {
+myForm.addEventListener('submit', function(event) {
   event.preventDefault();
+
   const inputs = document.querySelectorAll('.form-field');
-inputs.forEach((input) => {
-  input.value = '';
-});
-});
 
-
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+});
