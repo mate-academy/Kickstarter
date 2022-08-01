b66e4934ee31c61736652d31e79d4dae1926d0e5
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-08-01 15:09:22 +0300

    slider done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-'use strict';
+import Swiper, { Navigation } from 'swiper';
+
+// import styles bundle
+import 'swiper/swiper-bundle.min.css';
+
+// eslint-disable-next-line no-new
+new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  modules: [Navigation],
+});
