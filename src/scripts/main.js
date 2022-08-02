03d1b996a42709adf174a995fa3d34621d9b8acb
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-08-02 22:23:14 +0300

    Add Swiperjs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,27 @@
-'use strict';
+import Swiper from '../../node_modules/swiper/swiper-bundle.js';
+import '../../node_modules/swiper/swiper-bundle.css';
+
+// eslint-disable-next-line no-new
+new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
 document.getElementById('form').addEventListener('submit',
   (event) => {
