6be7febe69fee048120ba64d1ea9a0358170516b
Adrian Faryniuk <forever0754@gmail.com>
2022-08-02 14:58:49 +0300

    kikstarter 80% done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const tabs = document.querySelectorAll('.nav__link');
+
+tabs.forEach((tab, i) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(link => {
+      link.classList.remove('nav__link--is-active');
+    });
+
+    tabs[i].classList.add('nav__link--is-active');
+  });
+});
