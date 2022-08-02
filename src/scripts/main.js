4456a687ef794f9b244e2e108ad781700d2570a8
Dima Kobzar <kobzar7dima@gmail.com>
2022-08-02 18:25:56 +0300

    svg hover do not work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,14 @@
-// 'use strict';
-
-// core version + navigation, pagination modules:
 import Swiper, { Navigation, Pagination } from 'swiper';
-// import Swiper and modules styles
 import 'swiper/swiper-bundle.min.css';
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
 
 function addZero(num) {
   return (num > 9) ? num : '0' + num;
 }
 
-// addZero(1);
-
-// init Swiper:
 // eslint-disable-next-line no-new
 new Swiper('.swiper', {
-  // configure Swiper to use modules
   modules: [Navigation, Pagination],
+
   grabCursor: true,
   // указывает с какого слайда начинать
   // initialSlide: 3,
@@ -28,36 +18,26 @@ new Swiper('.swiper', {
 
   pagination: {
     el: '.swiper-pagination',
-    // el: '.test',
-    modifierClass: 'swiper-pagination-pag ',
-    // el: '.pag',
-    currentClass: 'start',
-    totalClass: 'end',
+
+    modifierClass: 'swiper-pagination-wrap ',
+
+    currentClass: 'swiper-current-slide',
+
+    totalClass: 'swiper-total-slides',
+
     type: 'fraction',
+
     formatFractionCurrent: addZero,
+
     formatFractionTotal: addZero,
 
   },
 
   navigation: {
-    // nextEl: '.swiper-button-next',
-    // prevEl: '.swiper-button-prev',
-    nextEl: '.test-next',
-    prevEl: '.test-prev',
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
   },
-
 });
-
-// const swiper = new Swiper('.mySwiper', {
-//   pagination: {
-//     el: '.swiper-pagination',
-//     type: 'fraction',
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#nav-menu') {
@@ -66,41 +46,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--scroll-remove');
   }
 });
-
-// const navActive = document.querySelector('.switchers__switch--active');
-// const switchers = document.querySelectorAll('.switchers__switch');
-
-// switchers.forEach(item => {
-//   item.addEventListener('click', () => {
-//     // console.log("object");
-//     if (item.classList.contains('.switchers__switch--active')) {
-//       item.classList.remove('switchers__switch--active');
-//     } else {
-//       item.classList.add('switchers__switch--active');
-//     }
-//   });
-// });
-
-// switchers.forEach(item => {
-//   item.addEventListener('click', (target) => {
-//     // console.log("object");
-//     // console.log(target);
-//     if (target.classList.contains('switchers__switch--active')) {
-//       target.classList.toggle('switchers__switch--active');
-//     }
-//   });
-// });
-
-// swiper попытки
-// const swiper = new Swiper('.mySwiper', {
-//   pagination: {
-//     el: '.swiper-pagination',
-//     type: 'fraction',
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
-
-// alert(document.querySelector('.slider__list').children.length);
