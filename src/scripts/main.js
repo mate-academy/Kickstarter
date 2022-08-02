7a7f5d7997306762205e9f7cb0749590c1d298fa
Sasha Kolomiiets <kinbean444@gmail.com>
2022-08-02 12:06:18 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 const tabs = document.querySelectorAll('.nav__link');
 
 tabs.forEach((tab, i) => {
