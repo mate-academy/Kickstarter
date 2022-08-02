c50bad28d58ea4a5278b4eddbdad6e843a8a7d0e
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-08-02 11:13:54 +0300

    problem was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
-'use strict';
+// eslint-disable-next-line no-unused-vars
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/swiper-bundle.min.css';
+
+// eslint-disable-next-line no-new, no-unused-vars
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  modules: [Navigation, Pagination],
+  // Navigation arrows
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
