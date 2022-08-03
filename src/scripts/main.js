3d4f003316492065434e28491e0f8c35f41f6fbf
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-08-03 16:13:41 +0300

    animation added and landing done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable padding-line-between-statements */
+/* eslint-disable no-var */
 // eslint-disable-next-line no-unused-vars
 import Swiper, { Navigation, Pagination } from 'swiper';
 // import Swiper and modules styles
@@ -26,3 +28,28 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  e.target.reset();
+}
+);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.about-us');
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', reveal);
