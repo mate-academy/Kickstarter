26fba022f4096a56666af8297aa8d4f5c752af71
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-08-04 14:46:00 +0300

    fixed by checklist

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,3 +15,18 @@ new Swiper('.swiper', {
   },
   modules: [Navigation],
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.form-send');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
