a4b1f58af24a3eddb497e8af58261a74c9bdf7b9
Shmelyov Oleg <shmelyov.oleg13@gmail.com>
2022-08-04 15:28:38 +0300

    added swiper carusel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
-'use strict';
+/* eslint-disable-next-line */
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
