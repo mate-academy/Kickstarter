1abe14f06e705ece83b64c96dae65572de00d460
Vadym Zahazei <vadkvadrat@gmail.com>
2022-08-21 20:53:06 +0300

    features slider implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,39 @@
-'use strict';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+
+function addZero(num) {
+  return (num > 9) ? num : '0' + num;
+}
+
+// eslint-disable-next-line no-new
+new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+
+  grabCursor: true,
+  rewind: true,
+
+  pagination: {
+    el: '.swiper__pagination',
+
+    modifierClass: 'swiper__pagination-wrap',
+
+    currentClass: 'swiper__current-slide',
+
+    totalClass: 'swiper__total-slides',
+
+    type: 'fraction',
+
+    formatFractionCurrent: addZero,
+
+    formatFractionTotal: addZero,
+
+  },
+
+  navigation: {
+    nextEl: '.swiper__arrow-next',
+    prevEl: '.swiper__arrow-prev',
+  },
+});
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
