0b2af22bc4a3b9e54cf756ca9d11d8f134339661
Max Bovtun <maxbovtun@gmail.com>
2022-08-26 13:45:44 +0300

    first mobile slider + pagination created in html, css and js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+let slideIndex = 1;
+const buttonPlus = document.querySelector('button[name=slide-plus]');
+const buttonMinus = document.querySelector('button[name=slide-minus]');
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  const x = document.getElementsByClassName('benefits-mobile__item');
+  const y = document.getElementsByClassName('benefits-mobile__pag');
+
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+    y[i].style.backgroundColor = '#cfeff0';
+  }
+  x[slideIndex - 1].style.display = 'flex';
+  y[slideIndex - 1].style.backgroundColor = '#0db2b3';
+}
+
+showDivs(slideIndex);
+
+buttonPlus.addEventListener('click', () => {
+  plusDivs(1);
+});
+
+buttonMinus.addEventListener('click', () => {
+  plusDivs(-1);
+});
