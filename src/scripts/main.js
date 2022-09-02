e39ab59878af80b2343bf2e34e4ae9e3e96d9dc3
Dmytri Stepanov <wootjkeee@gmail.com>
2022-09-02 20:12:20 +0300

    adapted header for tablet, added mobile nav burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// PREVENT PAGE RELOAD WHEN SUBMIT //
+
+const form = document.querySelector('.contact__form');
+const inputs = document.querySelectorAll('.contact__field');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
+
+// BURGER MENU //
+
+const mobileNavButon = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+
+mobileNavButon.addEventListener('click', function() {
+  mobileNavIcon.classList.toggle('mobile-nav-button__icon--active');
+  mobileNav.classList.toggle('mobile-nav--active');
+  document.body.classList.toggle('page__body--no-scroll');
+});
