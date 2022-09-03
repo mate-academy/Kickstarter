fc36799908515acf238ac4d8bcfd0fce73774e4e
Artem Musii <musiy.artem@gmail.com>
2022-09-03 21:44:56 +0300

    benefits section created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// eslint-disable-next-line no-undef
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+
+  mousewheel: {
+    releaseOnEdges: true,
+    forceToAxis: true,
+  },
+
+  on: {
+    slideChange: function() {
+      setTimeout(function() {
+        swiper.params.mousewheel.releaseOnEdges = false;
+      }, 500);
+    },
+
+    reachEnd: function() {
+      setTimeout(function() {
+        swiper.params.mousewheel.releaseOnEdges = true;
+      }, 750);
+    },
+
+    reachBeginning: function() {
+      setTimeout(function() {
+        swiper.params.mousewheel.releaseOnEdges = true;
+      }, 750);
+    },
+  },
+
+  touchReleaseOnEdges: true,
+
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true,
+  },
+});
