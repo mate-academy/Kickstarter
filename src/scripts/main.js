36bbe54948f1a5b0a3793552569db4977a3b886e
Maksym845 <mzuravel845@gmail.com>
2022-09-04 15:05:06 +0300

    header content was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,13 @@ window.addEventListener('hashchange', () => {
   const langU = document.querySelectorAll('.menu__language-ua');
   const langE = document.querySelectorAll('.menu__language-en');
 
-  if (window.location.hash === '#ua') {
+  if (window.location.hash === '#ua' || window.location.hash === '#nav-ua') {
     for (let i = 0; i < langU.length; i++) {
       langU[i].classList.add('menu__language-ua--active');
       langE[i].classList.add('menu__language-en--disabled');
     }
-  } else if (window.location.hash === '#en') {
+  } else if (window.location.hash === '#en'
+  || window.location.hash === '#nav-en') {
     for (let i = 0; i < langU.length; i++) {
       langU[i].classList.remove('menu__language-ua--active');
       langE[i].classList.remove('menu__language-en--disabled');
