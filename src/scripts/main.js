1fa5a2e98e5013e491b0a0932acd7112eb36acce
Yelisieiev Kim <rbv271@outlook.com>
2022-09-05 15:14:52 +0300

    benefits, presetation, about us, tecnology sections added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const slider = document.querySelector('.swiper');
+/* eslint-disable */
+new Swiper(slider, {
+/* eslint-enable */
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  autoplay: {
+    delay: 3000,
+  },
+});
