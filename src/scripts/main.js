6f1d4e357700f137c4f6725b8e5524fa6f536906
Artem Musii <musiy.artem@gmail.com>
2022-09-06 15:21:55 +0300

    contact-us section added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -54,3 +54,24 @@ const swiper2 = new Swiper('.slider2', {
     type: 'fraction',
   },
 });
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__field');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
