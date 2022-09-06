c49a3a79f23dac869e9fc2aa2a10d05e47ad939f
poznianski <poznianski@gmail.com>
2022-09-06 20:22:57 +0300

    added swiper

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// eslint-disable-next-line no-undef
+swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
