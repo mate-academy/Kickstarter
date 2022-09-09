0f0692e2ed7f6619df6a94dbb6150d6b4002ac86
Dmytri Stepanov <wootjkeee@gmail.com>
2022-09-09 12:43:39 +0300

    added slider to benefits section, added interval to features slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -39,13 +39,13 @@ window.addEventListener('hashchange', () => {
 
 // FEATURES SLIDER //
 
-const list = document.querySelector('.features__article');
-const prev = document.querySelector('.features__btn-left');
-const next = document.querySelector('.features__btn-right');
-const counter = document.querySelector('.features__counter');
+const featureList = document.querySelector('.features__article');
+const featurePrev = document.querySelector('.features__btn-left');
+const featureNext = document.querySelector('.features__btn-right');
+const featureCounter = document.querySelector('.features__counter');
 
 let currentPosition = 1;
-const count = list.children.length;
+const count = featureList.children.length;
 
 function moveTo(position) {
   currentPosition = position;
@@ -55,17 +55,71 @@ function moveTo(position) {
   } else if (currentPosition < 0) {
     currentPosition = count - 1;
   }
-  counter.textContent = '0' + (currentPosition + 1).toString();
+  featureCounter.textContent = '0' + (currentPosition + 1).toString();
 
-  list.style.transform = `translateX(${-currentPosition * 100}%)`;
+  featureList.style.transform = `translateX(${-currentPosition * 100}%)`;
 }
 
-next.addEventListener('click', () => {
+featureNext.addEventListener('click', () => {
   moveTo(currentPosition + 1);
 });
 
-prev.addEventListener('click', () => {
+featurePrev.addEventListener('click', () => {
   moveTo(currentPosition - 1);
 });
 
+setInterval(() => {
+  moveTo(currentPosition + 1);
+}, 2000);
+
 // BENEFITS SLIDER //
+
+const benefitsList = document.querySelector('.benefits__slider-list');
+const benefitsPrev = document.querySelector('.benefits__prev');
+const benefitsNext = document.querySelector('.benefits__next');
+const benefitsDots = document.querySelector('.benefits__dot-container');
+
+let benefitPosition = 0;
+const benefitsCount = benefitsList.children.length;
+
+for (let i = 0; i < benefitsDots.children.length; i++) {
+  benefitsDots.children[i].addEventListener('click', () => {
+    moveBenefits(i);
+  });
+}
+
+function highLightDot(newPosition) {
+  const activeDot = benefitsDots.querySelector('.benefits__dot--active');
+
+  if (activeDot) {
+    activeDot.classList.remove('benefits__dot--active');
+  }
+
+  const selectedDot = benefitsDots.children[newPosition];
+
+  selectedDot.classList.add('benefits__dot--active');
+}
+
+function moveBenefits(pos) {
+  benefitPosition = pos;
+
+  if (benefitPosition > benefitsCount - 1) {
+    benefitPosition = 0;
+  } else if (benefitPosition < 0) {
+    benefitPosition = benefitsCount - 1;
+  }
+
+  highLightDot(benefitPosition);
+
+  benefitsList.style.transform = `translateX(${-benefitPosition * 100}%)`;
+}
+
+benefitsNext.addEventListener('click', () => {
+  moveBenefits(benefitPosition + 1);
+});
+
+benefitsPrev.addEventListener('click', () => {
+  moveBenefits(benefitPosition - 1);
+});
+
+moveBenefits(0);
