ee8835d3af408a56092c057e66e79354283c24e4
George.kom <komolovua@gmail.com>
2022-09-11 02:26:30 +0300

    Header menu fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const menuOpenButton = document.querySelector('.icon--menu');
+const menuCloseButton = document.querySelector('.icon--close');
+const navbarMobile = document.querySelector('.navbar-mobile');
+
+const formFields = document.querySelectorAll('.form__field');
+const formSubmit = document.querySelector('.form__button');
+
+// ========= Menu Events ================
+menuOpenButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  navbarMobile.classList.toggle('navbar-mobile--active');
+});
+
+menuCloseButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  navbarMobile.classList.toggle('navbar-mobile--active');
+});
+
+// ============ Form Event ==============
+formSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  formFields.forEach(input => {
+    input.value = '';
+  });
+});
