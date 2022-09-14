32ee1db4d7734b04dcf956da9b5e1136a55324f8
Sasha Kornieiev <doktoralex8808@gmail.com>
2022-09-14 15:39:57 +0300

    kickstarter2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,15 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   form.reset();
 });
+
+const backToTop = document.querySelector('#backToTop');
+
+backToTop.style.display = 'none';
+
+document.querySelector('body').onscroll = function() {
+  if (window.scrollY > 400) {
+    backToTop.style.display = 'block';
+  } else {
+    backToTop.style.display = 'none';
+  }
+};
