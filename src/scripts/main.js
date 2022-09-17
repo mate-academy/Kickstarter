fd66caf44f7210c20f73312c0ec3bd87941f5ace
Yuri Gatskovskyi <yurimoreau@gmail.com>
2022-09-18 00:24:46 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const form = document.querySelector('.contact-form__form');
+const textarea = document.querySelector('.form-field--texterea');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  textarea.value = '';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
