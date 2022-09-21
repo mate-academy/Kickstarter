52ea9d25e290c43b4fc5c5237e692a11a7c0d09f
Burii Oleh <pirate.captainoleg@gmail.com>
2022-09-21 12:51:15 +0300

    Tablet was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.questions__form');
+const inputs = document.querySelectorAll('.form__field');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  inputs.forEach(function(elem) {
+    elem.value = '';
+  });
+});
