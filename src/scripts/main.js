746cc866480f3a205482f3ff027563fb60b31afb
George.kom <komolovua@gmail.com>
2022-09-28 01:13:49 +0300

    Fixed requested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,11 @@
 const menuOpenButton = document.querySelector('.icon--menu');
 const menuCloseButton = document.querySelector('.icon--close');
 const navbarMobile = document.querySelector('.navbar-mobile');
+const navbarMobileLinks = document.querySelector('.navbar-mobile__inner');
 
 const formFields = document.querySelectorAll('.form__field');
-const formSubmit = document.querySelector('.form__button');
+// const formSubmit = document.querySelector('.form__button');
+const form = document.querySelector('.form');
 
 // ========= Menu Events ================
 menuOpenButton.addEventListener('click', (e) => {
@@ -20,9 +22,14 @@ menuCloseButton.addEventListener('click', (e) => {
   document.body.classList.toggle('page__body--scroll');
 });
 
+navbarMobileLinks.addEventListener('click', (e) => {
+  navbarMobile.classList.toggle('navbar-mobile--active');
+  document.body.classList.toggle('page__body--scroll');
+});
+
 // ============ Form Event ==============
 
-formSubmit.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   formFields.forEach(input => {
