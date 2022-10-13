71ee005c6f89f4b662e2ab58e7675d777f0b5925
Yaroslav Dudar <dudar.komod@gmail.com>
2022-10-13 20:09:21 +0300

    Several section was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// eslint-disable-next-line no-undef, no-unused-vars
+const swiper = new Swiper('.features__slider', {
+
+  navigation: {
+    nextEl: '.features__button--next',
+    prevEl: '.features__button--prev',
+  },
+
+  loop: true,
+
+  pagination: {
+    el: '.features__pagination',
+    type: 'fraction',
+  },
+});
