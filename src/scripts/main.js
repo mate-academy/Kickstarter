b475f6b45258fe9fb4b0b7de54f5559603afbfd2
Maryna Mazur <marinemazur@gmail.com>
2022-11-11 22:56:13 +0200

    added slider for features section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,53 @@
 'use strict';
+
+function getFeaturesSlider() {
+  const featuresSliderWrapper = document.querySelector(
+    '.features__slider-wrapper'
+  );
+  const featuresButtonLeft = document.querySelector('.features-button__left');
+  const featuresButtonRight = document.querySelector('.features-button__rigth');
+  const featuresButtonPaginationStart = document.querySelector(
+    '.features-button__pagination-start'
+  );
+
+  let offset = 0;
+
+  featuresButtonRight.addEventListener('click', () => {
+    if (+featuresButtonPaginationStart.innerHTML > 0
+      && +featuresButtonPaginationStart.innerHTML < 3) {
+      const newValue = +featuresButtonPaginationStart.innerHTML + 1;
+
+      featuresButtonPaginationStart.innerHTML = `0${newValue}`;
+    }
+
+    if (offset < 520 && offset >= 0) {
+      offset += 260;
+    } else {
+      return;
+    }
+
+    featuresSliderWrapper.style.left = `-${offset}px`;
+  });
+
+  featuresButtonLeft.addEventListener('click', () => {
+    if (+featuresButtonPaginationStart.innerHTML > 1
+      && +featuresButtonPaginationStart.innerHTML <= 3) {
+      const newValue = +featuresButtonPaginationStart.innerHTML - 1;
+
+      featuresButtonPaginationStart.innerHTML = `0${newValue}`;
+    }
+
+    if (offset <= 520 && offset > 0) {
+      offset -= 260;
+    } else {
+      return;
+    }
+
+    featuresSliderWrapper.style.left = `-${offset}px`;
+  });
+};
+
+if (document.documentElement.clientWidth >= 320
+  && document.documentElement.clientWidth < 1280) {
+  getFeaturesSlider();
+};
