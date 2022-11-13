8007c2ac59c6dca852c82ac2dde69eefaa609e44
Oleksandr Filo <filo.sanya@gmail.com>
2022-11-13 15:57:22 +0200

    section get-in-touch and scroll-to-top btn were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+  form.reset();
+});
+
+// Show arrow to top
+
+const showArrow = document.getElementById('scrollTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 500) {
+    showArrow.classList.add('page__scroll-to-top--show');
+  } else {
+    showArrow.classList.remove('page__scroll-to-top--show');
+  }
+});
