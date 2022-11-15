07653d8e8257f47f956d44c767c82d1a84f4d362
Maryna Mazur <marinemazur@gmail.com>
2022-11-15 21:17:57 +0200

    changed html, added styles and js scripts for Tablet version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 'use strict';
+// slider for features section
 
 function getFeaturesSlider() {
   const featuresSliderWrapper = document.querySelector(
@@ -52,6 +53,7 @@ if (document.documentElement.clientWidth >= 320
   getFeaturesSlider();
 };
 
+// scripts for form
 const questionsForm = document.querySelector('.questions__form');
 
 questionsForm.addEventListener('submit', (event) => {
@@ -68,3 +70,58 @@ formFieldInput.addEventListener('change', (event) => {
     formButton.classList.add('form__button--active');
   }
 });
+
+// onTable scripts
+if (document.documentElement.clientWidth >= 768) {
+  const headerNavigation = document.querySelector('.header__navigation');
+
+  headerNavigation.hidden = false;
+
+  // change position for benefits__image
+  const copyBenefitImageWrapper = document.querySelector(
+    '.benefits__image-wrapper'
+  ).cloneNode(true);
+
+  document.querySelector(
+    '.benefits__image-wrapper'
+  ).remove();
+
+  const benefitsContent = document.querySelector('.benefits__content');
+
+  benefitsContent.after(copyBenefitImageWrapper);
+
+  // delete class swiper on technology
+  const swiper = document.querySelector('.swiper');
+
+  swiper.classList.remove('swiper');
+
+  // change position first block__title
+  const copyFirtstBlockTitle = document.querySelector('.block__title')
+    .cloneNode(true);
+  const blockContentFirst = document.querySelector('.block__content');
+
+  document.querySelector('.block__title').remove();
+  blockContentFirst.prepend(copyFirtstBlockTitle);
+
+  // change positions second block__title & picture.block__img
+
+  const copyBlockTitleLarger = document.querySelector('.block__title--larger')
+    .cloneNode(true);
+  const blockContentSecond = document.querySelectorAll('.block__content')[1];
+
+  document.querySelectorAll('.block__title')[1].remove();
+  blockContentSecond.prepend(copyBlockTitleLarger);
+
+  const copySecondPictureBlockImg = document.querySelectorAll(
+    'picture.block__img'
+  )[1].cloneNode(true);
+
+  document.querySelectorAll('picture.block__img')[1].remove();
+  blockContentSecond.after(copySecondPictureBlockImg);
+
+  // add container for features section
+  const containerDisabled = document.querySelector('.container--disabled');
+
+  containerDisabled.classList.remove('container--disabled');
+  containerDisabled.classList.add('container');
+}
