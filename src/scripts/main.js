b004147ebb54367f1dbffdc0592d8cccd9726954
Maryna Mazur <marinemazur@gmail.com>
2022-11-21 21:08:08 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
-// slider for features section
 
+// slider for features section
 function getFeaturesSlider() {
   const featuresSliderWrapper = document.querySelector(
     '.features__slider-wrapper'
@@ -55,14 +55,15 @@ if (document.documentElement.clientWidth >= 320
 
 // scripts for form
 const questionsForm = document.querySelector('.questions__form');
+const formButton = document.querySelector('.form__button');
+const formFieldInput = document.querySelector('.form__field--input');
 
 questionsForm.addEventListener('submit', (event) => {
   event.preventDefault();
   questionsForm.reset();
+  formButton.classList.remove('form__button--active');
+  formButton.classList.add('form__button--disabled');
 });
-
-const formButton = document.querySelector('.form__button');
-const formFieldInput = document.querySelector('.form__field--input');
 
 formFieldInput.addEventListener('change', (event) => {
   if (event.target.value) {
@@ -70,54 +71,3 @@ formFieldInput.addEventListener('change', (event) => {
     formButton.classList.add('form__button--active');
   }
 });
-
-// onTable scripts
-if (document.documentElement.clientWidth >= 768) {
-  // change position for benefits__image
-  const copyBenefitImageWrapper = document.querySelector(
-    '.benefits__image-wrapper'
-  ).cloneNode(true);
-
-  document.querySelector(
-    '.benefits__image-wrapper'
-  ).remove();
-
-  const benefitsContent = document.querySelector('.benefits__content');
-
-  benefitsContent.after(copyBenefitImageWrapper);
-
-  // delete class swiper on technology
-  const swiper = document.querySelector('.swiper');
-
-  swiper.classList.remove('swiper');
-
-  // change position first block__title
-  const copyFirtstBlockTitle = document.querySelector('.block__title')
-    .cloneNode(true);
-  const blockContentFirst = document.querySelector('.block__content');
-
-  document.querySelector('.block__title').remove();
-  blockContentFirst.prepend(copyFirtstBlockTitle);
-
-  // change positions second block__title & picture.block__img
-
-  const copyBlockTitleSecond = document.querySelector('.block__title--second')
-    .cloneNode(true);
-  const blockContentSecond = document.querySelectorAll('.block__content')[1];
-
-  document.querySelectorAll('.block__title')[1].remove();
-  blockContentSecond.prepend(copyBlockTitleSecond);
-
-  const copySecondPictureBlockImg = document.querySelectorAll(
-    'picture.block__img'
-  )[1].cloneNode(true);
-
-  document.querySelectorAll('picture.block__img')[1].remove();
-  blockContentSecond.after(copySecondPictureBlockImg);
-
-  // add container for features section
-  const containerDisabled = document.querySelector('.container--disabled');
-
-  containerDisabled.classList.remove('container--disabled');
-  containerDisabled.classList.add('container');
-}
