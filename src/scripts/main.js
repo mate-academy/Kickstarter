07a3fb7e7956edfe366df2902dac825683c1ab20
zynkd <coding707@mailbox.org>
2022-12-16 14:02:25 +0100

    Added sliding menu for small (mobile) screens

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// Sliding mobile menu
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu-mobile') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 // Prevent from submitting to (and refreshing) the page
 const formHomepageBottom = document.querySelector('#form-homepage-bottom');
 const inputs = document.querySelectorAll('.form__field');
