1e95d827049538c683322a41b7fecd9ed81ad966
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-01-16 16:44:40 +0200

    First release. Landing without slider for phones

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// Show menu
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// Show arrow to top
+
+const showArrow = document.querySelector('.arrow-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 450) {
+    showArrow.classList.add('arrow-to-top--show');
+  } else {
+    showArrow.classList.remove('arrow-to-top--show');
+  }
+});
