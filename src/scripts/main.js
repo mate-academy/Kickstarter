f763877c6c6bf9517785799e2be1b8b9d0256805
Natali <swetswet1234@gmail.com>
2023-03-07 18:49:18 +0200

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const $ = window.$;
+
+$(function() {
+  $('#myImgl').on('click', 'li', function(event) {
+    $(this).addClass('header__item--active').siblings()
+      .removeClass('header__item--active');
+    event.preventDefault();
+  });
+});
