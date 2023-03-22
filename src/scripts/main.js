fd64e386683b7be05219d4b54909b01b0da09e2f
Vladislav Tsymbalist <vladislavtsimba1999@gmail.com>
2023-03-22 18:43:35 +0300

    was added footer benefits about presentation blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('page__section--animation');
+    }
+  });
+}
+
+const options = { threshold: [0.5] };
+// eslint-disable-next-line no-undef
+const observer = new IntersectionObserver(onEntry, options);
+const elements = document.querySelectorAll('.page__section');
+
+for (const elm of elements) {
+  observer.observe(elm);
+}
