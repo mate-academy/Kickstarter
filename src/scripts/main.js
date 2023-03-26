ec4e6f210a4dbbe722812b2fdd3c4f1c722a6c5d
Daniel <henio09@wp.pl>
2023-03-26 18:28:46 +0200

    created a runaway button for contact form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
-'use strict';
+"use strict";
+
+const button = document.querySelector('.contact__send');
+const wrapper = document.querySelector('.contact__button-wrapper');
+const emailInput = document.querySelector('#email-input');
+const messageInput = document.querySelector('#message-input');
+
+button.addEventListener('mouseenter', () => {
+  wrapper.addEventListener('mousemove', moveButton);
+});
+
+button.addEventListener('mouseleave', () => {
+  wrapper.removeEventListener('mousemove', moveButton);
+});
+
+function moveButton(event) {
+  if (!emailInput.checkValidity()) {
+    emailInput.reportValidity();
+
+    const wrapperRect = wrapper.getBoundingClientRect();
+    const buttonRect = button.getBoundingClientRect();
+    const mouseX = event.clientX;
+
+    const buttonWidth = buttonRect.width;
+    const wrapperWidth = wrapperRect.width;
+
+    let newPosition = 0;
+
+    if (mouseX > buttonRect.left + buttonWidth / 2) {
+      newPosition = buttonRect.left - wrapperRect.left - buttonWidth;
+    } else {
+      newPosition = buttonRect.left - wrapperRect.left + buttonWidth;
+    }
+
+    if (newPosition < 0) {
+      newPosition = wrapperWidth - buttonWidth;
+    } else if (newPosition + buttonWidth > wrapperWidth) {
+      newPosition = 0;
+    }
+
+    button.style.left = newPosition + 'px';
+  }
+}
