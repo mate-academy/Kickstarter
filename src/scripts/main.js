32f049bc4fd56122f7011e20b08526a25fb61c77
Ir-ra <chapter.ku@ukr.net>
2023-03-28 12:58:33 +0300

    add arrow-up + vars + hovers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,9 +8,32 @@ window.addEventListener('hashchange', () => {
   }
 });
 
+// button submit
 const form = document.getElementById('kickaster_form');
 
 form.addEventListener('submit', function handleSubmit(event) {
   event.preventDefault();
   form.reset();
+});
+
+
+// button up
+document.addEventListener("DOMContentLoaded", () => {
+  let to_top_btn = document.querySelector(".icon--up");
+
+  window.onscroll = function () {
+      if (window.pageYOffset > 360) {
+          to_top_btn.style.display = "block"
+      } else {
+          to_top_btn.style.display = "none"
+      }
+  }
+
+  // smooth scroll up
+  to_top_btn.addEventListener("click", function () {
+      window.scrollBy({
+          top: -document.documentElement.scrollHeight,
+          behavior: "smooth"
+      });
+  });
 });
