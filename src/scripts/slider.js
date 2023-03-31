6df6b7279009ee7904db869ec5d6cfb44de58fe0
domkab <dominykas.bobinas@gmail.com>
2023-03-31 22:47:52 +0300

    1.3.0 features section started

---
+++ src/scripts/slider.js
@@ -0,0 +1,35 @@
+// 'use strict'
+
+// ;(function ($) {
+//   $(document).ready(function () {
+//     $(document).ready(function () {
+//       let currentFeatureIndex = 0
+//       const features = $('.feature')
+
+//       $('.features__navigation--forward').click(function (e) {
+//         e.preventDefault()
+//         // Hide the current feature by sliding it to the left
+//         $(features[currentFeatureIndex]).css('left', '-100%')
+
+//         // Increment the feature index
+//         currentFeatureIndex = (currentFeatureIndex + 1) % features.length
+
+//         // Show the next feature by sliding it in from the right
+//         $(features[currentFeatureIndex]).css('left', '0')
+//       })
+
+//       $('.features__navigation--back').click(function (e) {
+//         e.preventDefault()
+//         // Hide the current feature by sliding it to the right
+//         $(features[currentFeatureIndex]).css('left', '100%')
+
+//         // Decrement the feature index
+//         currentFeatureIndex =
+//           (currentFeatureIndex - 1 + features.length) % features.length
+
+//         // Show the previous feature by sliding it in from the left
+//         $(features[currentFeatureIndex]).css('left', '0')
+//       })
+//     })
+//   })
+// })(jQuery)
