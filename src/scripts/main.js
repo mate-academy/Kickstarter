a5bb6df02f9d4c55bcf98fffefe08edc86f09b44
Sergey Shapovalenko <jackshkiper@gmail.com>
2023-03-31 13:59:22 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    loop: true,
+
+    pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    currentClass: 'features-slider__pagination-current',
+    totalClass: 'features-slider__pagination-total',
+  },
+
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+    scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+    
+  });
+  
