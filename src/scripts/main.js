4f43c1563681108e63afb4d28fa2976263aabd55
Andrey Bikir <bikir.a.v@gmail.com>
2023-04-02 19:06:36 +0300

    Crazybaby

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// swiper
+/* global Swiper */
+
+const swiper = new Swiper('.swiper', {
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+swiper.enable();
+
+// form-reset
+const form = document.getElementById('form-reload');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target.reset();
+});
