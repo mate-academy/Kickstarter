60ce17babb31c86bfdc85226f4cb30413b2242b6
domkab <dominykas.bobinas@gmail.com>
2023-05-18 15:17:37 +0300

    1.3.4 fixed hero, and carousel now has issue with opacity transition

--- src/scripts/featuresSwitch.js
+++ src/scripts/featuresSwitch.js
@@ -34,11 +34,57 @@ function switchFeature(direction) {
   }
 
   features.forEach(function(feature) {
-    feature.style.display
-      = feature.getAttribute('data-feature') == currentFeature
-        ? 'block'
-        : 'none';
+    if (feature.getAttribute('data-feature') == currentFeature) {
+      feature.classList.add('active');
+    } else {
+      feature.classList.remove('active');
+    }
   });
 
   currentFeatureElement.textContent = String(currentFeature).padStart(2, '0');
 }
+
+// switchFeature();
+
+// let currentFeature = 1;
+// const totalFeatures = 3;
+
+// const features = document.querySelectorAll('.features__feature');
+// const currentFeatureElement = document.querySelector(
+//   '.features__navigation-count-number--current'
+// );
+
+// document
+//   .querySelector('.features__navigation-arrows-link--back')
+//   .addEventListener('click', function(e) {
+//     e.preventDefault();
+//     switchFeature(-1);
+//   });
+
+// document
+//   .querySelector('.features__navigation-arrows-link--forward')
+//   .addEventListener('click', function(e) {
+//     e.preventDefault();
+//     switchFeature(1);
+//   });
+
+// function switchFeature(direction) {
+//   currentFeature += direction;
+
+//   if (currentFeature > totalFeatures) {
+//     currentFeature = 1;
+//   }
+
+//   if (currentFeature < 1) {
+//     currentFeature = totalFeatures;
+//   }
+
+//   features.forEach(function(feature) {
+//     feature.style.display
+//       = feature.getAttribute('data-feature') == currentFeature
+//         ? 'block'
+//         : 'none';
+//   });
+
+//   currentFeatureElement.textContent = String(currentFeature).padStart(2, '0');
+// }
