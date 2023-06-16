2cf7b9df7d05d3738f12242452fb4a95e48fb5b7
Sergey Arnautov <sirius9312@ukr.net>
2023-06-16 23:14:26 +0300

    CRAZYBABY-LANDING

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const page = document.querySelector('.page');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    page.style.overflowY = 'hidden';
+  } else {
+    page.style.overflowY = 'scroll';
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
