c2bc04602e5280d747f7cdf0173d7b71d984fba3
Sergey Arnautov <sirius9312@ukr.net>
2023-06-17 23:56:36 +0300

    CRAZYBABY-LANDING-FIXED

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,6 @@
 'use strict';
 
-const page = document.querySelector('.page');
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    page.style.overflowY = 'hidden';
-  } else {
-    page.style.overflowY = 'scroll';
-  }
-});
-
-const form = document.querySelector('form');
+const form = document.querySelector('.footer__form');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
