b5a28a0f3ee736df78007f0d1b21ebc6f7fbb3c9
Tetyana Shatrova <shatrova.tetyana@gmail.com>
2023-06-29 20:05:39 +0300

    version 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const frm = document.getElementById('submitForm');
+
+frm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // frm.submit(); // Submit the form
+  frm.reset(); // Reset all form data
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
