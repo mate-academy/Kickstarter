3d551393490c1416dc9c62eee03e2ed02db0ffcb
andrew gidrovich <mr.androvich55555@gmail.com>
2023-07-01 22:26:10 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const form = document.querySelector('.question__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
+
+window.addEventListener('hashchange', toggleScroll);
+
+function toggleScroll() {
+  const body = document.body;
+  const menu = document.getElementById('page-menu');
+  const menuComputedStyle = window.getComputedStyle(menu);
+
+  if (menuComputedStyle.opacity === '1') {
+    body.style.overflow = 'auto';
+  } else {
+    body.style.overflow = 'hidden';
+  }
+}
+
+
