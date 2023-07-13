8563edb40f4069639bf7993a478240b3ef9d2247
Dmytro Betsan <dmytro.betsan@gmail.com>
2023-07-13 16:31:14 +0200

    slider and language color fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,44 +27,6 @@ window.addEventListener('scroll', () => {
   }
 });
 
-// changing lists
-
-const afterButton = document.querySelector('.list__button--after');
-const beforeButton = document.querySelector('.list__button--before');
-
-const lists = document.querySelectorAll('.features__list');
-const listPages = document.querySelectorAll('.list__page');
-
-let activeListIndex = 0;
-
-afterButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  switchToList(activeListIndex + 1);
-});
-
-beforeButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  switchToList(activeListIndex - 1);
-});
-
-function switchToList(index) {
-  lists[activeListIndex].classList.remove('list--active');
-  lists[activeListIndex].classList.add('list--inactive');
-  listPages[activeListIndex].classList.remove('list__page--active');
-
-  activeListIndex = index;
-
-  if (activeListIndex >= lists.length) {
-    activeListIndex = 0;
-  } else if (activeListIndex < 0) {
-    activeListIndex = lists.length - 1;
-  }
-
-  lists[activeListIndex].classList.remove('list--inactive');
-  lists[activeListIndex].classList.add('list--active');
-  listPages[activeListIndex].classList.add('list__page--active');
-}
-
 // reset form after submission
 
 const form = document.querySelector('#form, #form--ukr');
@@ -79,11 +41,146 @@ function buttonClick(event) {
 // menu classes added for shop and overlay
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
+  if (window.location.hash === '#menu'
+  || window.location.hash === '#menu--ukr') {
     document.body.classList.add('page__body--with-menu');
     document.getElementById('overlay').classList.add('overlay--active');
   } else {
     document.body.classList.remove('page__body--with-menu');
     document.getElementById('overlay').classList.remove('overlay--active');
   }
+});
+
+// slider eng
+document.addEventListener('DOMContentLoaded', function() {
+  const beforeButton = document.getElementById('before-button');
+  const afterButton = document.getElementById('after-button');
+  const list1 = document.getElementById('feature-list-1');
+  const list2 = document.getElementById('feature-list-2');
+  const list3 = document.getElementById('feature-list-3');
+  const page1 = document.querySelector('.list__page--active');
+  const page2 = document.querySelectorAll('.list__page')[1];
+  const page3 = document.querySelectorAll('.list__page')[2];
+
+  beforeButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (list1.classList.contains('list--active')) {
+      list1.classList.remove('list--active');
+      list1.classList.add('list--inactive');
+      list3.classList.remove('list--inactive');
+      list3.classList.add('list--active');
+      page1.classList.remove('list__page--active');
+      page3.classList.add('list__page--active');
+    } else if (list2.classList.contains('list--active')) {
+      list2.classList.remove('list--active');
+      list2.classList.add('list--inactive');
+      list1.classList.remove('list--inactive');
+      list1.classList.add('list--active');
+      page2.classList.remove('list__page--active');
+      page1.classList.add('list__page--active');
+    } else if (list3.classList.contains('list--active')) {
+      list3.classList.remove('list--active');
+      list3.classList.add('list--inactive');
+      list2.classList.remove('list--inactive');
+      list2.classList.add('list--active');
+      page3.classList.remove('list__page--active');
+      page2.classList.add('list__page--active');
+    }
+  });
+
+  afterButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (list1.classList.contains('list--active')) {
+      list1.classList.remove('list--active');
+      list1.classList.add('list--inactive');
+      list2.classList.remove('list--inactive');
+      list2.classList.add('list--active');
+      page1.classList.remove('list__page--active');
+      page2.classList.add('list__page--active');
+    } else if (list2.classList.contains('list--active')) {
+      list2.classList.remove('list--active');
+      list2.classList.add('list--inactive');
+      list3.classList.remove('list--inactive');
+      list3.classList.add('list--active');
+      page2.classList.remove('list__page--active');
+      page3.classList.add('list__page--active');
+    } else if (list3.classList.contains('list--active')) {
+      list3.classList.remove('list--active');
+      list3.classList.add('list--inactive');
+      list1.classList.remove('list--inactive');
+      list1.classList.add('list--active');
+      page3.classList.remove('list__page--active');
+      page1.classList.add('list__page--active');
+    }
+  });
+});
+
+// slider ukr
+
+document.addEventListener('DOMContentLoaded', function() {
+  const page1 = document.querySelector('.list__page--active');
+  const page2 = document.querySelectorAll('.list__page')[1];
+  const page3 = document.querySelectorAll('.list__page')[2];
+
+  const beforeButtonUkr = document.getElementById('before-button--ukr');
+  const afterButtonUkr = document.getElementById('after-button--ukr');
+  const list1ukr = document.getElementById('feature-list-1--ukr');
+  const list2ukr = document.getElementById('feature-list-2--ukr');
+  const list3ukr = document.getElementById('feature-list-3--ukr');
+
+  beforeButtonUkr.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (list1ukr.classList.contains('list--active')) {
+      list1ukr.classList.remove('list--active');
+      list1ukr.classList.add('list--inactive');
+      list3ukr.classList.remove('list--inactive');
+      list3ukr.classList.add('list--active');
+      page1.classList.remove('list__page--active');
+      page3.classList.add('list__page--active');
+    } else if (list2ukr.classList.contains('list--active')) {
+      list2ukr.classList.remove('list--active');
+      list2ukr.classList.add('list--inactive');
+      list1ukr.classList.remove('list--inactive');
+      list1ukr.classList.add('list--active');
+      page2.classList.remove('list__page--active');
+      page1.classList.add('list__page--active');
+    } else if (list3ukr.classList.contains('list--active')) {
+      list3ukr.classList.remove('list--active');
+      list3ukr.classList.add('list--inactive');
+      list2ukr.classList.remove('list--inactive');
+      list2ukr.classList.add('list--active');
+      page3.classList.remove('list__page--active');
+      page2.classList.add('list__page--active');
+    }
+  });
+
+  afterButtonUkr.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (list1ukr.classList.contains('list--active')) {
+      list1ukr.classList.remove('list--active');
+      list1ukr.classList.add('list--inactive');
+      list2ukr.classList.remove('list--inactive');
+      list2ukr.classList.add('list--active');
+      page1.classList.remove('list__page--active');
+      page2.classList.add('list__page--active');
+    } else if (list2ukr.classList.contains('list--active')) {
+      list2ukr.classList.remove('list--active');
+      list2ukr.classList.add('list--inactive');
+      list3ukr.classList.remove('list--inactive');
+      list3ukr.classList.add('list--active');
+      page2.classList.remove('list__page--active');
+      page3.classList.add('list__page--active');
+    } else if (list3ukr.classList.contains('list--active')) {
+      list3ukr.classList.remove('list--active');
+      list3ukr.classList.add('list--inactive');
+      list1ukr.classList.remove('list--inactive');
+      list1ukr.classList.add('list--active');
+      page3.classList.remove('list__page--active');
+      page1.classList.add('list__page--active');
+    }
+  });
 });
