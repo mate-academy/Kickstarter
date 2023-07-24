f48e16e2a252068bcbdc01a8cc868f55b7b90e96
Rodionsav <rodionsav@mate.academy>
2023-07-24 19:14:29 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const { default: Swiper } = require('swiper');
+
+Swiper('.text-slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
