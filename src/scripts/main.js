5894eccea8c1e1ad1c49942ae234851f58a852a2
Fedos17 <dfkd81@gmail.com>
2023-07-28 18:32:35 +0300

    adds block benefits

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
-'use strict';
+// 'use strict';
+
+import Flickity from 'flickity';
+
+/* eslint-disable no-unused-vars */
+const flkty = new Flickity('.main-gallery', {
+  // options
+  cellAlign: 'center',
+  wrapAround: true,
+  autoPlay: 2000,
+  contain: true,
+  watchCSS: true,
+  selectedAttraction: 0.05,
+  friction: 0.3,
+  prevNextButtons: false,
+});
+/* eslint-enable no-unused-vars */
+
+window.onhashchange = (e) => {
+  const menu = e.newURL.includes('#menu')
+    ? document.body.classList.add('page-overflow')
+    : document.body.classList.remove('page-overflow');
+
+  return menu;
+};
