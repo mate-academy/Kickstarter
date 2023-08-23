c40b652d93ff6d8210b52ec2d93d89f999d72215
Mykola Huryn <mukola197022@gmail.com>
2023-08-23 20:23:28 +0200

    Kickstarter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const form = document.querySelector('.footer__form');
+
+const sendButton = document.querySelector(
+  '.form__button'
+);
+
+sendButton.addEventListener('click', e => {
+  form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
