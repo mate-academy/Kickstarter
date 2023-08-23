faeb230071eb9399d4b31b15c29e5148e1e5bc5f
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-08-23 16:30:57 +0300

    Fixed after @loralevitska review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// Show menu
-
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('menu--on');
@@ -10,9 +8,8 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-// Show arrow to top
-
 const showArrow = document.querySelector('.arrow-to-top');
+const contactUsForm = document.querySelector('.contact-us__form');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 450) {
@@ -20,4 +17,64 @@ window.addEventListener('scroll', () => {
   } else {
     showArrow.classList.remove('arrow-to-top--show');
   }
+});
+
+contactUsForm.addEventListener('submit', event => {
+  contactUsForm.reset();
+  event.preventDefault();
+});
+
+// eslint-disable-next-line no-undef, no-unused-vars
+const swiper = new Swiper('.slider1', {
+  direction: 'vertical',
+
+  mousewheel: {
+    releaseOnEdges: true,
+    forceToAxis: true,
+  },
+
+  on: {
+    slideChange: function() {
+      setTimeout(function() {
+        swiper.params.mousewheel.releaseOnEdges = false;
+      }, 500);
+    },
+
+    reachEnd: function() {
+      setTimeout(function() {
+        swiper.params.mousewheel.releaseOnEdges = true;
+      }, 750);
+    },
+
+    reachBeginning: function() {
+      setTimeout(function() {
+        swiper.params.mousewheel.releaseOnEdges = true;
+      }, 750);
+    },
+  },
+
+  touchReleaseOnEdges: true,
+
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true,
+  },
+});
+
+// eslint-disable-next-line no-undef, no-unused-vars
+const swiper2 = new Swiper('.slider2', {
+
+  navigation: {
+    nextEl: '.features__swiper-button-next',
+    prevEl: '.features__swiper-button-prev',
+  },
+
+  loop: true,
+
+  pagination: {
+    el: '.features__swiper-pagination',
+    type: 'fraction',
+  },
 });
