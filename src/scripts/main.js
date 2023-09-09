a83835fe08ebf5bf2444413ce5a3f9593c7b986b
vlad lutsyk <lutsyk710@gmail.com>
2023-09-09 18:30:36 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.getElementById('next').onclick = function() {
+  const widthItem = document.querySelector('.features__card-main').offsetWidth;
+
+  document.getElementById('features__id').scrollLeft += widthItem;
+};
+
+document.getElementById('prev').onclick = function() {
+  const widthItem = document.querySelector('.features__card-main').offsetWidth;
+
+  document.getElementById('features__id').scrollLeft -= widthItem;
+};
