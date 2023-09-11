e7ce0ea3b678dcc81af43c9c8ef2320dfc14a87e
Anastasiia Em <ema199625@gmail.com>
2023-09-11 17:22:36 +0200

    Initial commit - header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function toggleLang() {
+  lang.forEach(item => item.classList.toggle('toolbar__lang--chosen'));
+}
+
+const lang = document.querySelectorAll('.toolbar__lang');
+
+lang.forEach(item => item.addEventListener('click', toggleLang));
