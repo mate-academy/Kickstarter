7df7e98495606115a547a61854817bb2fd279a65
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2023-09-19 15:09:47 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.getElementById('form');
+const userEmail = document.getElementById('user-email');
+const userMessage = document.getElementById('user-message');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  userEmail.value = '';
+  userMessage.value = '';
+});
