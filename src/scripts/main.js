935c1595d5722884da1d00e22bdb558645ae3e97
Inna Sheremet <innsheremet88@gmail.com>
2023-09-22 13:14:25 +0300

    add sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function changeClassContainer() {
+  const about = document.getElementById('about');
+  const technology = document.getElementById('technology');
+
+  if (window.innerWidth >= 1440) {
+    about.classList.add('container');
+    technology.classList.add('container');
+  } else {
+    about.classList.remove('container');
+    technology.classList.remove('container');
+  }
+};
+
+changeClassContainer();
+window.addEventListener('resize', changeClassContainer);
