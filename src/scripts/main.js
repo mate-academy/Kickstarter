a2768026a45d191b05757c5eea9546c7cbb55b50
Vitalii Drabynko <vpdrabynko@gmail.com>
2023-09-28 12:18:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
+
+document.getElementById('touch').addEventListener('submit', function(event) {
+  event.preventDefault();
+  validateForm();
+});
+
+function validateForm() {
+  const emailInput = document.getElementById('email');
+  const email = emailInput.value;
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (!emailPattern.test(email)) {
+    return;
+  };
+
+  emailInput.value = '';
+  document.getElementById('message').value = '';
+};
