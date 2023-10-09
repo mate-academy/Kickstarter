756125532d4dfc351add9f1800098ee5c86b28de
Mykhailiuk Viktor <117285070+Viktor535x@users.noreply.github.com>
2023-10-10 00:59:58 +0300

    test1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const form = document.querySelector('#form');
+
+function submit(event) {
+  event.preventDefault();
+  form.reset();
+}
+
+form.addEventListener('submit', submit);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menuR') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
