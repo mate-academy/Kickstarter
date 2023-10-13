013fc24d34f5fda0a45c0a39f79848b2330e4bb8
LukashevychSergey <lukashevychsergey@gmail.com>
2023-10-13 15:49:21 +0300

    made the changes according to the instructions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 'use strict';
+// prevent sending the form
 
 const form = document.getElementById('myForm');
 
@@ -6,4 +7,17 @@ form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   form.reset();
+});
+
+// hiding the scroll when menu opens
+
+const openMenuButton = document.getElementById('openMenu');
+const closeMenuButton = document.getElementById('closeMenu');
+
+openMenuButton.addEventListener('click', () => {
+  document.body.style.overflow = 'hidden';
+});
+
+closeMenuButton.addEventListener('click', () => {
+  document.body.style.overflow = 'auto';
 });
