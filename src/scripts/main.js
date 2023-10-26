da017aa145ac7809823a0efee27bd77b9d661069
Malyholovka Ostap <maligolovkaostap@gmail.com>
2023-10-26 12:32:10 +0300

    added correct variable names

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const scrollTopButton = document.querySelector('.footer__arrow');
+
+document.addEventListener('scroll', () => {
+  if (document.documentElement.scrollTop > 500) {
+    scrollTopButton.style.pointerEvents = 'auto';
+    scrollTopButton.style.opacity = '1';
+  } else {
+    scrollTopButton.style.opacity = '0';
+    scrollTopButton.style.pointerEvents = 'none';
+  }
+});
