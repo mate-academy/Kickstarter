7db7eaa99582b7aadfbf05e39b1b09ab891f00f8
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2023-11-06 18:17:33 +0200

    finish whole page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const goTopButton = document.querySelector('.page__go-top');
+
+document.addEventListener('scroll', () => {
+  if (document.documentElement.scrollTop > 500) {
+    goTopButton.style.pointerEvents = 'auto';
+    goTopButton.style.opacity = '1';
+  } else {
+    goTopButton.style.opacity = '0';
+    goTopButton.style.pointerEvents = 'none';
+  }
+});
