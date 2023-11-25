242eac8d7f63f5eee175370af8efe02765808571
Natalia Matash <natalimatash@gmail.com>
2023-11-25 23:08:04 +0200

    Create menu section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,26 @@
 'use strict';
 
-const langEn = document.querySelector('.top__languigeEN');
-const langUa = document.querySelector('.top__languigeUA');
+const langEn = document.querySelector('.options__languigeEN');
+const langUa = document.querySelector('.options__languigeUA');
 
 langEn.addEventListener('click', () => {
-  langEn.classList.add('top__languigeEN--active');
-  langEn.classList.remove('top__languigeEN--passive');
-  langUa.classList.add('top__languigeUA--passive');
-  langUa.classList.remove('top__languigeUA--active');
+  langEn.classList.add('options__languigeEN--active');
+  langEn.classList.remove('options__languigeEN--passive');
+  langUa.classList.add('options__languigeUA--passive');
+  langUa.classList.remove('options__languigeUA--active');
 });
 
 langUa.addEventListener('click', () => {
-  langEn.classList.add('top__languigeEN--passive');
-  langEn.classList.remove('top__languigeEN--active');
-  langUa.classList.add('top__languigeUA--active');
-  langUa.classList.remove('top__languigeUA--passive');
+  langEn.classList.add('options__languigeEN--passive');
+  langEn.classList.remove('options__languigeEN--active');
+  langUa.classList.add('options__languigeUA--active');
+  langUa.classList.remove('options__languigeUA--passive');
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
