a97f1da37ac56522b6c634ac4d753bdb21a5b3c3
Natalia Matash <natalimatash@gmail.com>
2023-11-25 17:55:46 +0200

    Update header info with effects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const langEn = document.querySelector('.top__languigeEN');
+const langUa = document.querySelector('.top__languigeUA');
+
+langEn.addEventListener('click', () => {
+  langEn.classList.add('top__languigeEN--active');
+  langEn.classList.remove('top__languigeEN--passive');
+  langUa.classList.add('top__languigeUA--passive');
+  langUa.classList.remove('top__languigeUA--active');
+});
+
+langUa.addEventListener('click', () => {
+  langEn.classList.add('top__languigeEN--passive');
+  langEn.classList.remove('top__languigeEN--active');
+  langUa.classList.add('top__languigeUA--active');
+  langUa.classList.remove('top__languigeUA--passive');
+});
