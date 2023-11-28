3ea3dddc9c36fe1293dc0a1c0d9a41603031cfce
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-11-28 15:21:02 +0200

    form submit and some hovers added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Initialize variables
   let slideIndex = 0;
   const slides = document.querySelectorAll('.slider__item');
   const prevButton = document.querySelector('.slider__button-prev');
@@ -55,5 +54,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
   prevButton.addEventListener('click', () => {
     showPreviousSlide();
+  });
+});
+
+const contactForm = document.getElementById('contact__form');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  window.alert('This form has been successfully submitted!');
+  contactForm.reset();
+});
+
+const links = document.querySelectorAll('.advantages__nav-link');
+
+links[0].style.fontWeight = 'bold';
+
+links.forEach(function(link) {
+  link.addEventListener('focus', function() {
+    links[0].style.fontWeight = 'normal';
+    this.style.fontWeight = 'bold';
+  });
+
+  link.addEventListener('blur', function() {
+    this.style.fontWeight = 'normal';
   });
 });
