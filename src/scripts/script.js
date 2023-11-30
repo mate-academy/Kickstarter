3a8533fdcbc8e7b0c5fc657edb949c0a8fb36948
Oleksandr <d43z61p0@gmail.com>
2023-11-30 18:39:01 +0200

    adding styles, js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,32 @@
-function test() {
-  alert(1);
+let state = 0;
+const content = document.getElementsByClassName("section__7-content");
+const pageNumber = document.getElementById("slide-curr");
+
+document.getElementById("left-arrow").addEventListener("click", function() {
+  content[state].classList.remove("active");
+  if(state === 0) {
+    state = 2;
+  } else {
+    state--;
+  }
+
+  renderContent();
+  event.preventDefault();
+});
+
+document.getElementById("right-arrow").addEventListener("click", function() {
+  content[state].classList.remove("active");
+  if(state === 2) {
+    state = 0;
+  } else {
+    state++;
+  }
+
+  renderContent();
+  event.preventDefault();
+})
+
+function renderContent() {
+  content[state].classList.add("active");
+  pageNumber.innerHTML = "0" + (state + 1);
 }
