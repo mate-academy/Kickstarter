418b8b238a35962459c0ffc40d25dccb0bb08e3f
Natalia Matash <natalimatash@gmail.com>
2023-12-05 14:32:06 +0200

    Add all sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const langEn = document.querySelector('.options__languigeEN');
+const langUa = document.querySelector('.options__languigeUA');
+
+langEn.addEventListener('click', () => {
+  langEn.classList.add('options__languigeEN--active');
+  langEn.classList.remove('options__languigeEN--passive');
+  langUa.classList.add('options__languigeUA--passive');
+  langUa.classList.remove('options__languigeUA--active');
+});
+
+langUa.addEventListener('click', () => {
+  langEn.classList.add('options__languigeEN--passive');
+  langEn.classList.remove('options__languigeEN--active');
+  langUa.classList.add('options__languigeUA--active');
+  langUa.classList.remove('options__languigeUA--passive');
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.questions__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
