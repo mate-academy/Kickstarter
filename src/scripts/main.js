b127777332d2e797bb98cad93ae611e0a93b0a6b
Mykola Ostapchuk <koliaostap4uk1991@gmail.com>
2024-01-10 21:45:26 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const sliderButtons = document.querySelector('.slider__button-wrap');
+const slides = document.querySelectorAll('.slide');
+const currentNum = document.querySelector('.slider__num--current');
+
+sliderButtons.addEventListener('click', moveSlide);
+
+let counter = 1;
+
+function moveSlide({ target }) {
+  const left = target.classList.contains('slider__button--right');
+
+  slides.forEach(slide => {
+    slide.style.display = 'none';
+  });
+
+  switch (true) {
+    case left === true:
+      if (counter === 3) {
+        counter = 1;
+      } else {
+        counter++;
+      }
+
+      changeSlideNumber();
+      break;
+
+    default:
+      if (counter === 1) {
+        counter = 3;
+      } else {
+        counter--;
+      }
+
+      changeSlideNumber();
+  }
+}
+
+function changeSlideNumber() {
+  document.querySelector(`.slide--${counter}`).style.display = 'block';
+  currentNum.textContent = `0${counter}`;
+}
