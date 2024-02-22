458fb21c18f6818588593c88062fe4cb7368e414
Oleh Prokopenko <olehprokopenko1@gmail.com>
2024-02-22 20:05:33 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// eslint-disable-next-line no-undef, no-unused-vars
+const swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
