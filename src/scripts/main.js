18e39f617f5d05d7884ca0edfc7549f20fc04c00
Denys Khvashchenko <superdened@gmail.com>
2024-02-27 16:49:11 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,58 @@
-'use strict';
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const pageReload = document.querySelector('#form-reload');
+
+pageReload.addEventListener('submit', function formReload(event) {
+  event.preventDefault();
+  pageReload.reset();
+});
+
+let count = 0;
+
+const sliderLine = document.querySelector('.slider-line');
+const currentSlider = document.querySelector('.slider__buttons__count-current');
+
+document
+  .querySelector('.slider__buttons__button-right')
+  .addEventListener('click', () => {
+    count += 260;
+
+    if (count > 520) {
+      count = 0;
+    }
+
+    sliderLine.style.left = -count + 'px';
+    updateCurrentSlider();
+  });
+
+document
+  .querySelector('.slider__buttons__button-left')
+  .addEventListener('click', () => {
+    count -= 260;
+
+    if (count < 0) {
+      count = 520;
+    }
+
+    sliderLine.style.left = -count + 'px';
+    updateCurrentSlider();
+  });
+
+function updateCurrentSlider() {
+  switch (count) {
+    case 260:
+      currentSlider.textContent = '02';
+      break;
+    case 520:
+      currentSlider.textContent = '03';
+      break;
+    default:
+      currentSlider.textContent = '01';
+  }
+}
