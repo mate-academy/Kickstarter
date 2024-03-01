0e3fe3e132265f3c011a08b8709470ad87fe0fa0
Valentyn Sobol <valiksobol14@gmail.com>
2024-03-01 23:09:06 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+window.addEventListener('hashchange', function() {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page-body');
+  } else {
+    document.body.classList.remove('page-body');
+  }
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
