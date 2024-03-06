265cbab25d349aa22a83b1d179ad1fc588d8928f
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-03-06 23:23:57 +0200

    correct mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const menuListItems = document.querySelectorAll('.nav__list');
+const closeButton = document.querySelector('.icon--close');
+const menuButton = document.querySelector('.icon--menu');
+
+document.addEventListener('DOMContentLoaded', function() {
+  menuButton.addEventListener('click', function() {
+    document.querySelector('.page').style.overflow = 'hidden';
+    document.querySelector('.page').classList.add('lock-scrollbar');
+  });
+
+  closeButton.addEventListener('click', function() {
+    document.querySelector('.page').style.overflow = 'auto';
+    document.querySelector('.page').classList.remove('lock-scrollbar');
+  });
+
+  menuListItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      document.querySelector('.page').style.overflow = 'auto';
+      document.querySelector('.page').classList.remove('lock-scrollbar');
+    });
+  });
+});
