5103ddefc43a8050203255bc78664627ed791007
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-03-10 17:20:04 +0200

    implement header section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const lang = document.querySelector('.specs__link');
+/* global alert */
+
+lang.addEventListener('click', () => {
+  alert('The feature is still under development');
+});
