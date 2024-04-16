a1502336c744c0cd7ea1321d2419ddd493364452
Hanna <hmatviienko@gmail.com>
2024-04-16 18:14:16 +0300

    solution

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Kickstarter landing page (HARD)
 
-**⚠️ Attention: this is a hard landing page for development. We highly recommend choosing this landing page only if you didn't have a lot of troubles with previous homework tasks and if you sent all homework in time (didn't miss deadlines).** 
+**⚠️ Attention: this is a hard landing page for development. We highly recommend choosing this landing page only if you didn't have a lot of troubles with previous homework tasks and if you sent all homework in time (didn't miss deadlines).**
 
-Implement landing page according to [Figma design](https://www.figma.com/file/Ujp7bCFuvuJlkn8TSbQPSZ/%E2%84%9611-(kickstarter)?node-id=19655%3A33) - Use BEM and SCSS
+Implement landing page according to [Figma design](https://www.figma.com/file/Ujp7bCFuvuJlkn8TSbQPSZ/%E2%84%9~611-(kickstarter)?node-id=19655%3A33) - Use BEM and SCSS
 - The design 1440px
 - Desktop 1280px
 - Tablet 640px
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,65 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const prevButton = document.querySelector('.features__buttons--back');
+  const nextButton = document.querySelector('.features__buttons--forward');
+  const featureBlocks = document.querySelectorAll('.features__block');
+  const counter = document.querySelector('.features__counter');
+
+  let currentSlide = 2;
+  const totalSlides = featureBlocks.length;
+
+  showSlide(currentSlide);
+
+  updateCounter();
+  showAllBlocks();
+
+  function showSlide(index) {
+    featureBlocks.forEach((block, i) => {
+      if (i === index) {
+        block.style.display = 'flex';
+      } else {
+        block.style.display = 'none';
+      }
+    });
+    updateCounter();
+  }
+
+  function showAllBlocks() {
+    if (window.innerWidth >= 1256) {
+      featureBlocks.forEach((block) => {
+        block.style.display = 'flex';
+      });
+    }
+  }
+
+  function updateCounter() {
+    counter.textContent = (currentSlide + 1) + ' / ' + totalSlides;
+  }
+
+  prevButton.addEventListener('click', function() {
+    currentSlide = (currentSlide - 1 + featureBlocks.length)
+      % featureBlocks.length;
+    showSlide(currentSlide);
+  });
+
+  nextButton.addEventListener('click', function() {
+    currentSlide = (currentSlide + 1) % featureBlocks.length;
+    showSlide(currentSlide);
+  });
+
+  function toggleSlider() {
+    if (window.innerWidth >= 1256) {
+      prevButton.style.display = 'none';
+      nextButton.style.display = 'none';
+    } else {
+      prevButton.style.display = 'block';
+      nextButton.style.display = 'block';
+    }
+  }
+  toggleSlider();
+
+  window.addEventListener('resize', function() {
+    toggleSlider();
+  });
+});
