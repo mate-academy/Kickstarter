d63e29f4a74e0759badbda9e5e1164747daa51a7
Oleksandra Kuziura <radioday2014@gmail.com>
2024-07-13 19:58:36 +0300

    implement slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.button--prev');
+const nextButton = document.querySelector('.button--next');
+const slides = Array.from(slider.querySelectorAll('.item--slider'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  document.querySelector('.count').innerText = `0${(slideIndex + 1)}`;
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  document.querySelector('.count').innerText = `0${slideIndex + 1}`;
+  updateSlider();
+}
+
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+updateSlider();
