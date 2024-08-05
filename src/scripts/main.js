f33d25b70ae680fffb0144883f74b85940bd602c
user <mariiaantoniv@gmail.com>
2024-08-05 23:03:50 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+var tetxSwiper = new Swiper('.slider-content',{
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  loop: true,
+  speed: 700,
+  autoplay: {
+    deplay: 900,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'fraction',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  }
+});
