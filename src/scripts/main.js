c8a250d1488829b1f2ede3bbae7925ce3f40a86c
Savka Hanna <savka.hanna.mac@gmail.com>
2024-09-03 15:14:34 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+var swiper = new Swiper(".mySwiper", {
+  autoplay:{
+    delay: 3000,
+  },
+    loop: true,
+});
+
+var swiper = new Swiper(".mySwiperFeatures", {
+  autoplay:{
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+});
