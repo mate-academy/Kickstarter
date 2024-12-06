26de19961ff5224e2779174c3d38ab1af2a7b87d
Maksym <makstarasovv1@gmail.com>
2024-12-06 14:49:18 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,40 @@
 'use strict';
+'use strict';
+
+let swiper = new Swiper('.mySwiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    renderFraction: function (currentClass, totalClass) {
+      return `<span class="${currentClass}"></span> / <span class="${totalClass}"></span>`;
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+swiper.on('paginationUpdate', function (swiper, paginationEl) {
+  const currentEl = paginationEl.querySelector('.swiper-pagination-current');
+  const totalEl = paginationEl.querySelector('.swiper-pagination-total');
+
+  if (currentEl && totalEl) {
+    currentEl.textContent = String(currentEl.textContent).padStart(2, '0');
+    totalEl.textContent = String(totalEl.textContent).padStart(2, '0');
+  }
+});
+
+//..............................................
+
+const optionsLanguage = document.querySelector('.options__language');
+
+optionsLanguage.addEventListener('click', () => {
+  const enLang = optionsLanguage.querySelector('.en');
+  const uaLang = optionsLanguage.querySelector('.ua');
+
+  // Переключаем активный класс
+  enLang.classList.toggle('active');
+  uaLang.classList.toggle('active');
+});
