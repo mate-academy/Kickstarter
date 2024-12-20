3bd7d60d43aec822fbf33abfdc70aece1d4679fe
Lidiia Shvets <lepoki051003@gmail.com>
2024-12-20 20:04:09 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+/* global Swiper */ // Додаємо, щоб ESLint не скаржився на невідомий Swiper
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.features__swiper', {
+  loop: true, // Зациклення
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    init: function() {
+      const totalSlides = this.slides.length;
+
+      document.querySelector('.swiper-pagination-total').textContent
+      = totalSlides.toString().padStart(2, '0');
+
+      const currentSlide = this.realIndex + 1;
+
+      document.querySelector('.swiper-pagination-current').textContent
+      = currentSlide.toString().padStart(2, '0');
+    },
+    slideChange: function() {
+      // Оновлюємо числову пагінацію
+      const currentSlide = this.realIndex + 1; // Поточний слайд
+
+      document.querySelector('.swiper-pagination-current').textContent
+      = currentSlide.toString().padStart(2, '0');
+    },
+  },
+});
