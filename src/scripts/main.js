ae384b4630ea4f8e0d659072dc894cec112fa440
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2025-01-21 23:03:42 +0200

    created a base landing template with the  main functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+import { burgerMenu } from './utils/burgerMenu';
+import { slider } from './utils/slider';
+
+document.addEventListener('DOMContentLoaded', () => {
+  burgerMenu();
+  slider();
+});
