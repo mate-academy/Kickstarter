6f28a6ba3b507c516e128a6011e604679ff042e0
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2025-02-16 21:50:27 +0200

    add language selection

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
+import { initLanguageSelector } from './modules/language-selector.js';
+
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.addEventListener('DOMContentLoaded', () => {
+  initLanguageSelector();
+});
---
+++ src/scripts/modules/translation.js
@@ -0,0 +1,26 @@
+export const translations = {
+  en: {
+    nav: {
+      ourStory: 'Our story',
+      aboutUs: 'About us',
+      features: 'Features',
+      getInTouch: 'Get in touch',
+    },
+    aside: {
+      specs: 'Specs',
+      button: 'Buy',
+    },
+  },
+  ua: {
+    nav: {
+      ourStory: 'Наша історія',
+      aboutUs: 'Про нас',
+      features: 'Особливості',
+      getInTouch: 'Контакти',
+    },
+    aside: {
+      specs: 'Характеристики',
+      button: 'Купити',
+    },
+  },
+};
