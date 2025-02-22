9e4a26d474b1f95d27c21a13e1a0f54edc9355de
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2025-02-22 19:43:42 +0200

    finish translation *everything except features block, 2 much work for small scale project*

--- src/scripts/modules/translation.js
+++ src/scripts/modules/translation.js
@@ -10,6 +10,54 @@ export const translations = {
       specs: 'Specs',
       button: 'Buy',
     },
+    main: {
+      benefits: {
+        benefit_1: {
+          title: 'Futuristic Design',
+          description: `Luna’s performance is balanced and smooth in all frequency ranges which makes the music both naturally pleasant and distinctly more layered.`,
+        },
+        benefit_2: {
+          title: 'Tweeter Speaker System',
+          description:
+            'To deliver a more layered sound performance better than a sole ull-range speaker, our team equipped Luna with one more tweeter speaker responsible for high-frequency sound independently.',
+        },
+        benefit_3: {
+          title: 'Multiroom System',
+          description:
+            'Luna is natively compatible with your home Wi-Fi. Set up multiple speakers in different rooms to expand your music experience into the entire house.',
+        },
+        benefit_4: {
+          title: 'Intuitive Lighting System',
+          description:
+            'An intuitive user interface allows you to adjust the hue and saturation of color for lighting that fits any mood and situation.',
+        },
+      },
+      aboutUs: {
+        card1: {
+          title: 'DESIGNED FOR THE FUTURE',
+          description:
+            'In 2014, a group of geeky industrial designers, engineering veterans and acoustic experts formed crazybaby. This is a bunch of passionate people who are crazy enough to think they can challenge the industry with disruptive audio products.',
+        },
+        card2: {
+          title: 'Luna Eye',
+          description:
+            'Luna Eye is to the essence of this innovative light and audio system. It comprises an independent tweeter speaker, a light guiding component, a dome and a ring ornament.',
+        },
+        link: 'See more about us',
+      },
+      quote: {
+        text: '“It really took me by surprise honestly to have such full beautiful sound that coming out of this small compact device. And with the brush aluminum surface, it feels so familiar. Like my iPhone.”',
+        name: 'Garrett Martin',
+        position: 'Creative Director',
+      },
+      features: {
+        title: 'FEATURES',
+      },
+      contacts: {
+        title: 'DO YOU HAVE ANY QUESTIONS?',
+        button: 'Send',
+      },
+    },
   },
   ua: {
     nav: {
@@ -21,6 +69,55 @@ export const translations = {
     aside: {
       specs: 'Характеристики',
       button: 'Купити',
+    },
+    main: {
+      benefits: {
+        benefit_1: {
+          title: 'Футуристичний дизайн',
+          description:
+            'Звучання Luna збалансоване та плавне у всіх частотних діапазонах, що робить музику природно приємною та більш багатошаровою.',
+        },
+        benefit_2: {
+          title: 'Система динаміків',
+          description:
+            'Щоб забезпечити більш якісне звучання порівняно з одним широкосмуговим динаміком, наша команда оснастила Luna додатковим твітером, який незалежно відповідає за високочастотний звук.',
+        },
+        benefit_3: {
+          title: 'Мультикімнатна система',
+          description:
+            'Luna сумісна з домашнім Wi-Fi. Встановіть кілька колонок у різних кімнатах, щоб розширити музичний досвід на весь будинок.',
+        },
+        benefit_4: {
+          title: 'Інтуїтивна система освітлення',
+          description:
+            'Інтуїтивно зрозумілий інтерфейс дозволяє регулювати відтінок та насиченість кольору освітлення відповідно до будь-якого настрою та ситуації.',
+        },
+      },
+      aboutUs: {
+        card1: {
+          title: 'СТВОРЕНО ДЛЯ МАЙБУТНЬОГО',
+          description:
+            'У 2014 році група захоплених промислових дизайнерів, досвідчених інженерів та експертів з акустики заснувала crazybaby. Це команда пристрасних людей, які достатньо божевільні, щоб кинути виклик індустрії інноваційними аудіопродуктами.',
+        },
+        card2: {
+          title: 'Luna Eye',
+          description:
+            'Luna Eye - це сутність цієї інноваційної світло-звукової системи. Вона включає незалежний твітер, компонент світлопровідності, купол та кільцевий орнамент.',
+        },
+        link: 'Дізнатися більше про нас',
+      },
+      quote: {
+        text: '"Мене справді здивувало, що такий повний, красивий звук виходить з цього компактного пристрою. А з поверхнею з матового алюмінію він відчувається таким знайомим. Як мій iPhone."',
+        name: 'Гаррет Мартін',
+        position: 'Креативний директор',
+      },
+      features: {
+        title: 'ОСОБЛИВОСТІ',
+      },
+      contacts: {
+        title: 'МАЄТЕ ЗАПИТАННЯ?',
+        button: 'Надіслати',
+      },
     },
   },
 };
