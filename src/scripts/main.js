2f693b284e198b2dc6184612341726a58c843112
dima <shymdima74@gmail.com>
2025-03-02 19:47:40 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector(".main__form");
+
+
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+
+        });
+
+});
