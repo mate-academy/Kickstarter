4cedfc0a7303116174ad68c622aafba143b1e2e2
maxmodrr <maxmodrr@gmail.com>
2025-04-05 17:10:21 +0300

    results

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const featuresContent = document.querySelector('.features__content');
+const slidesBlock = document.querySelector('.features__slides');
+const allSlides = [...document.querySelectorAll('.features__slide')];
+const pagination = document.querySelector('.features__pagination');
+const spanActiveSlide = document.querySelector('.features__active');
+
+const form = document.querySelector('.questions__form');
+
+let currentSlide = 0;
+const slideCount = allSlides.length - 1;
+
+pagination.addEventListener('click', (e) => {
+  const button = e.target.closest('button');
+
+  if (button) {
+    const stylesSlides = getComputedStyle(slidesBlock);
+    const widthOneSlide = parseInt(stylesSlides.width) / 3;
+
+    allSlides[currentSlide].classList.remove('features__slide--active');
+
+    if (button.classList.contains('features__button--right')) {
+      if (currentSlide < slideCount) {
+        currentSlide++;
+      }
+    } else {
+      if (currentSlide > 0) {
+        currentSlide--;
+      }
+    }
+
+    allSlides[currentSlide].classList.add('features__slide--active');
+    spanActiveSlide.innerHTML = `0${currentSlide + 1}`;
+    slidesBlock.style.marginLeft = `-${currentSlide * widthOneSlide}px`;
+  }
+});
+
+const observer = new ResizeObserver((entries) => {
+  for (let entry of entries) {
+    const newWidth = entry.contentRect.width;
+
+    slidesBlock.style.marginLeft = `-${currentSlide * newWidth}px`;
+  }
+});
+
+observer.observe(featuresContent);
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 1280 && window.location.hash.includes('#menu')) {
+    window.location.hash = '';
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const button = document.querySelector('.questions__button');
+  const inputs = document.querySelectorAll('.questions__input');
+  inputs.forEach((input) => input.classList.add('questions__input--done'));
+  button.setAttribute('disabled', 'true');
+  form.reset();
+});
