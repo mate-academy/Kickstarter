56f6cac506aa82aae41df3ef7d704c6cdbc83369
Diana Kadimbekova <lebedenko202@gmail.com>
2025-04-10 18:44:23 +0300

    commit no linter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const enBtn = document.querySelector('.top__lang--item-en');
+  const uaBtn = document.querySelector('.top__lang--item-ua');
+
+  async function changeLang(lang) {
+    try {
+      const response = await fetch(`../translate/${lang}.json`);
+
+      if (!response.ok) {
+        console.log('Error');
+      }
+
+      const texts = await response.json();
+
+      Object.entries(texts).forEach(([key, text]) => {
+        document.querySelectorAll(`[data-key="${key}"]`).forEach((el) => {
+          el.textContent = text;
+        });
+      });
+
+      localStorage.setItem('lang', lang);
+    } catch (error) {
+      console.log('Error');
+    }
+  }
+
+  enBtn?.addEventListener('click', () => changeLang('en'));
+  uaBtn?.addEventListener('click', () => changeLang('ua'));
+
+  changeLang(localStorage.getItem('lang') || 'en');
+});
