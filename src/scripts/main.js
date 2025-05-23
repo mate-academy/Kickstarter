b92afa37fd1e24c9e6c63d24a05825c301a1b602
diachkinainna <diachkinainna@gmail.com>
2025-05-23 19:36:07 +0300

    basic solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const open = document.querySelector('.top-bar__icon--menu');
+const close = document.querySelector('.top-bar__icon--close');
+const menu = document.querySelector('.menu');
+const scrollUp = document.querySelector('.body__scroll-up');
+
+open.addEventListener('click', () => {
+  body.classList.add('no-scroll');
+  scrollUp.classList.add('hidden');
+})
+
+close.addEventListener('click', () => {
+  body.classList.remove('no-scroll');
+  scrollUp.classList.remove('hidden');
+})
+
+menu.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    body.classList.remove('no-scroll');
+    scrollUp.classList.remove('hidden');
+  }
+})
