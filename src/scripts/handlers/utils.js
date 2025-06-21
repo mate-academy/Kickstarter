fe80efcc4c53fad5d3cb5d6da8a77c9bf8cd4cc4
Oleg Nepiuk <gtailde@gmail.com>
2025-06-21 05:33:41 +0200

    fix: update tablet breakpoint to 680px

--- src/scripts/handlers/utils.js
+++ src/scripts/handlers/utils.js
@@ -1,6 +1,6 @@
 export const getBreakpoint = () => {
   const width = window.innerWidth;
-  if (width >= 1280) return 'desktop';
-  if (width >= 780) return 'tablet';
+  if (width > 1280) return 'desktop';
+  if (width > 680) return 'tablet';
   return 'mobile';
 }
